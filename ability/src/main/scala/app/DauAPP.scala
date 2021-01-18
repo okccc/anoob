8b9f8f4917088742b41746e1e4b015eb6cb8e2f2
okccc <1573976179@qq.com>
2021-01-18 15:44:13 +0800

    kafka消费者实现数据幂等性

--- ability/src/main/scala/app/DauAPP.scala
+++ ability/src/main/scala/app/DauAPP.scala
@@ -1,108 +1,145 @@
 package app
 
-import java.lang
-
+import bean.DauInfo
 import com.alibaba.fastjson.{JSON, JSONObject}
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.apache.kafka.common.TopicPartition
 import org.apache.spark.SparkConf
+import org.apache.spark.rdd.RDD
 import org.apache.spark.streaming.dstream.{DStream, InputDStream}
+import org.apache.spark.streaming.kafka010.{HasOffsetRanges, OffsetRange}
 import org.apache.spark.streaming.{Seconds, StreamingContext}
 import redis.clients.jedis.Jedis
-import utils.{MyDateUtil, MyKafkaUtil, MyRedisUtil}
+import utils._
 
+import java.lang
 import scala.collection.mutable.ListBuffer
 
 /**
- * @author okccc
- * @date 2020/12/13 11:24
- * @desc 日活统计
+ * Author: okccc
+ * Date: 2020/12/13 11:24
+ * Desc: 日活统计
  */
 object DauAPP {
   def main(args: Array[String]): Unit = {
     // 创建spark配置信息
     val conf: SparkConf = new SparkConf().setMaster("local[*]").setAppName("DauApp")
-//      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
+      .set("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
     // 创建StreamingContext对象
     val ssc: StreamingContext = new StreamingContext(conf, Seconds(3))
     // 设置日志级别
     ssc.sparkContext.setLogLevel("warn")
 
     // 指定kafka的topic和groupId
-    val topic: String = "t_start"
-    val groupId: String = "aaa"
+    val topicName: String = "start"
+    val groupId: String = "g"
 
-    // ============================== 功能1.从kafka中读取数据 ==============================
-    val kafkaDStream: InputDStream[ConsumerRecord[String, String]] = MyKafkaUtil.getKafkaDStream(ssc, topic, groupId)
-    // 测试输出1
-    kafkaDStream.map((record: ConsumerRecord[String, String]) => record.value()).print(100)
-
-    // 转换DStream中的数据结构
-    val jsonDStream: DStream[JSONObject] = kafkaDStream.map((record: ConsumerRecord[String, String]) => {
-      // 获取ConsumerRecord的value部分 {"action":"1","ba":"Huawei","detail":"433","en":"start","t":"1607982026247"...}
+    // ============================== 功能1.SparkStreaming消费kafka数据=============================
+    // 1.从redis读取偏移量
+    val offsetMap: Map[TopicPartition, Long] = MyOffsetUtil.getOffset(topicName, groupId)
+    // 2.读取kafka数据
+    var recordDStream: InputDStream[ConsumerRecord[String, String]] = null
+    if(offsetMap != null && offsetMap.nonEmpty) {
+      // redis中已经有偏移量,就从偏移量处读取
+      recordDStream  = MyKafkaUtil.getKafkaDStream(ssc, topicName, groupId, offsetMap)
+    } else {
+      // redis中还没有偏移量,默认从latest处读取
+      recordDStream = MyKafkaUtil.getKafkaDStream(ssc, topicName, groupId)
+    }
+    // 测试输出
+//    recordDStream.print()
+    // ConsumerRecord(topic = start, partition = 0, leaderEpoch = 0, offset = 58001, CreateTime = 1610853239384, serialized key size = -1,
+    // serialized value size = 330, headers = RecordHeaders(headers = [], isReadOnly = false), key = null, value = {"":""...})
+    // 3.获取本批次数据所在分区对应偏移量的起始和结束位置
+    var offsetRanges: Array[OffsetRange] = Array.empty[OffsetRange]
+    val offsetDStream: DStream[ConsumerRecord[String, String]] = recordDStream.transform((rdd: RDD[ConsumerRecord[String, String]]) => {
+      // RDD是顶层父类,将其强制类型转换为实现了HasOffsetRange特质的子类,RDD的所有子类中只有KafkaRDD实现了该特质
+      offsetRanges = rdd.asInstanceOf[HasOffsetRanges].offsetRanges
+      rdd
+    })
+    // 测试输出
+//    offsetDStream.print()
+    // 4.转换DStream中的数据结构
+    val jsonDStream: DStream[JSONObject] = offsetDStream.map((record: ConsumerRecord[String, String]) => {
+      // 获取ConsumerRecord的value部分
       val jsonStr: String = record.value()
-      // 将字符串转换成json对象
+      println("key = " + record.key())  // key = null
+      println("value = " + record.value())  // value = {"action":"2","ar":"MX","ba":"HTC","vn":"1.2.1"...}
+      // 将json字符串封装成json对象
       val jsonObj: JSONObject = JSON.parseObject(jsonStr)
       // 获取时间戳
       val ts: lang.Long = jsonObj.getLong("t")
       // 转换成日期和小时
-      val str: String = MyDateUtil.parseUnixToDateTime(ts)
+      val dtStr: String = MyDateUtil.parseUnixToDateTime(ts)
       // 添加到json对象
-      jsonObj.put("dt", str.substring(0, 10))
-      jsonObj.put("hr", str.substring(11, 13))
+      jsonObj.put("dt", dtStr.substring(0, 10))
+      jsonObj.put("hr", dtStr.substring(11, 13))
       jsonObj
     })
-    // 测试输出2
-    jsonDStream.print()
+    // 测试输出
+//    jsonDStream.print()  // {"mid":"11","hr":"18","dt":"2021-01-16","uid":"11","en":"start"...}
 
-    // ============================== 功能2.通过redis对数据去重 ==============================
-    // 1).遍历元素
-//    val filterDStream: DStream[JSONObject] = jsonDStream.filter((jsonObj: JSONObject) => {
-//      // 获取日期和设备号,并将其拼接成保存到redis的key
-//      val dt: String = jsonObj.getString("dt")
-//      val mid: String = jsonObj.getString("mid")
-//      val dauKey: String = "dau:" + dt
-//      // 获取redis连接对象,每个元素都会建立一次连接,性能较差
-//      val jedis: Jedis = MyRedisUtil.getJedis
-//      // 往set集合添加键值对,1表示添加成功,0表示已存在
-//      val long: lang.Long = jedis.sadd(dauKey, mid)
-//      // 设置key的过期时间
-//      jedis.expire(dauKey, 3600 * 24)
-//      // 关闭连接
-//      jedis.close()
-//      if (long == 1) true else false
-//    })
-//    filterDStream.count().print()
-
-    // 2).遍历分区
-    val filterDStream: DStream[JSONObject] = jsonDStream.mapPartitions(mapPartFunc = (jsonObjIterator: Iterator[JSONObject]) => {
-      // 获取redis连接对象,有几个分区就建立几次连接,提高性能
-      val jedis: Jedis = MyRedisUtil.getJedisClient
-      // 创建存放首次登陆对象的列表缓冲区
-      val listBuffer: ListBuffer[JSONObject] = ListBuffer[JSONObject]()
-      // 遍历分区中的元素
-      for (jsonObj <- jsonObjIterator) {
-        // 获取日期和设备号,并将其拼接成保存到redis的key
+    // ============================== 功能2.利用redis过滤当日日活设备(去重) ==============================
+    // 实时计算中去重是常见需求,redis/mysql(性能一般)/spark updateStateByKey(checkpoint小文件问题)
+    // 1.遍历分区,涉及到数据库连接的操作,通常的是以分区为单位处理数据,减少数据库连接次数
+    val filteredDStream: DStream[JSONObject] = jsonDStream.mapPartitions((iterator: Iterator[JSONObject]) => {
+      // 2.获取jedis客户端,有几个分区就创建几次连接,提高性能
+      val jedis: Jedis = MyRedisUtil.getJedis()
+      // 存放首次登录用户的列表
+      val listBuffer: ListBuffer[JSONObject] = new ListBuffer[JSONObject]
+      // 3.遍历分区中的元素
+      iterator.foreach((jsonObj: JSONObject) => {
+        // 获取日期和设备号
         val dt: String = jsonObj.getString("dt")
         val mid: String = jsonObj.getString("mid")
+        // 将日期拼接成key
         val dauKey: String = "dau:" + dt
-        // 往set集合添加键值对,1表示添加成功,0表示已存在
+        // 往redis的set集合中添加数据,1表示添加成功,0表示已存在
         val long: lang.Long = jedis.sadd(dauKey, mid)
         // 设置key的过期时间
         jedis.expire(dauKey, 3600 * 24)
-        // 将首次登陆的对象添加到ListBuffer
-        if (long == 1) {
+        // 将首次登录用户添加到列表
+        if (long == 1L) {
           listBuffer.append(jsonObj)
         }
-      }
-      // 及时释放资源,不然连接池不够用 redis.clients.jedis.exceptions.JedisException: Could not get a resource from the pool
-      jedis.close()
+        // 释放资源,防止连接池不够用 redis.clients.jedis.exceptions.JedisException: Could not get a resource from the pool
+        jedis.close()
+      })
+      // 返回迭代器
       listBuffer.toIterator
     })
-    // 测试输出3
-    filterDStream.count().print()
+    // 测试输出
+//    filteredDStream.count().print()
 
-    // ============================== 功能3.将日活数据保存到es ==============================
-
+    // ============================== 功能3.将每批次新增的当日日活信息输出到es ==============================
+    // 1.输出操作通常由foreachRDD完成,RDD本身是一个集合,只不过存储的是逻辑抽象而不是具体数据
+    filteredDStream.foreachRDD((rdd: RDD[JSONObject]) => {
+      // 2.遍历分区,涉及数据库连接的操作,通常是以分区为单位处理数据,减少数据库连接次数
+      rdd.foreachPartition((iterator: Iterator[JSONObject]) => {
+        var dt: String = null
+        // 3.遍历每个分区中的元素
+        val dauList: List[(String, DauInfo)] = iterator.map((jsonObj: JSONObject) => {
+          // 4.封装DauInfo对象
+          val commonJsonObj: JSONObject = jsonObj.getJSONObject("common")
+          val uid: String = commonJsonObj.getString("uid")
+          val mid: String = commonJsonObj.getString("mid")
+          val ar: String = commonJsonObj.getString("ar")
+          val ch: String = commonJsonObj.getString("ch")
+          val vc: String = commonJsonObj.getString("vc")
+          dt = jsonObj.getString("dt")
+          val hr: String = jsonObj.getString("hr")
+          val mi: String = "0" // 日志中没有的字段可以给常量值
+          val ts: lang.Long = jsonObj.getLong("t")
+          val dauInfo: DauInfo = DauInfo(mid, uid, ar, ch, vc, dt, hr, mi, ts)
+          // 返回结果
+          (mid, dauInfo)
+        }).toList
+        // 5.往es批量插入数据
+        MyESUtil.bulkIndex("dau_" + dt, dauList)
+      })
+      // 6.保存偏移量到redis
+      MyOffsetUtil.saveOffset(topicName, groupId, offsetRanges)
+    })
 
     // 启动采集
     ssc.start()
