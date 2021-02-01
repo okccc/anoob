d77f7cbe884bb3938c043ca0c368b48643ae3f08
okccc <1573976179@qq.com>
2021-02-01 14:47:19 +0800

    hive底层null值问题

--- ability/src/main/java/bigdata/shell/mysql2hive.sh
+++ ability/src/main/java/bigdata/shell/mysql2hive.sh
@@ -20,26 +20,28 @@ watch_path=/home/hive/gds/filewatch/sqoop/input/${YESTERDAY}
 if [[ ! -d ${log_path} ]] ; then
     mkdir ${log_path}
 fi
-
 if [[ ! -d ${watch_path} ]] ; then
     mkdir ${watch_path}
 fi
 
-echo "${CURRENT} start" >> ${log_path}/${table}.log
-
-# 全量抽数据
-sqoop import --connect jdbc:mysql://${ip}:${port}/${mysql_db}?tinyInt1isBit=false
---username ${username} --password ${password} --table ${table} --fields-terminated-by '\001'
---hive-drop-import-delims --hive-import --hive-table base.${table} -m 4 --delete-target-dir --hive-overwrite
---null-string '\\N' --null-non-string '\\N' &>> ${log_path}/${table}.log
-
-# 增量抽数据
-sqoop import --connect jdbc:mysql://${ip}:${port}/${mysql_db}?tinyInt1isBit=false
---username ${username} --password ${password} --table ${table} --fields-terminated-by '\001' --where "create_time=${YESTERDAY} or update_time=${YESTERDAY}"
---hive-drop-import-delims --hive-import --hive-table base.${table} -m 4 --delete-target-dir --hive-overwrite --hive-partition-key dt --hive-partition-value ${YESTERDAY}
---null-string '\\N' --null-non-string '\\N' &>> ${log_path}/${table}.log
-
-echo "${CURRENT} end" >> ${log_path}/${table}.log
+# 数据导入
+sqoop import
+--connect jdbc:mysql://${ip}:${port}/${mysql_db}?tinyInt1isBit=false  # 数据库
+--username ${username}                                                # 用户名
+--password ${password}                                                # 密码
+--table ${table}                                                      # mysql表
+--fields-terminated-by '\001'                                         # 字段分隔符,注意区分'\001'和'\t'
+--where "create_time=${YESTERDAY} or update_time=${YESTERDAY}"        # 筛选数据(增量选项)
+--hive-drop-import-delims                                             # 过滤掉分隔符\n, \r, \01
+--hive-import                                                         # 导入hive,不设置就使用hive默认分隔符
+--hive-table base.${table}                                            # hive表
+-m 4                                                                  # -m表示map数量,sqoop任务没有reduce阶段
+--delete-target-dir                                                   # 删除已存在的目录
+--hive-overwrite                                                      # 覆盖数据
+--hive-partition-key dt                                               # 分区字段dt(增量选项)
+--hive-partition-value ${YESTERDAY}                                   # 分区值(增量选项)
+--null-string '\\N'                                                   # 将字符串空值写成\N
+--null-non-string '\\N' &>> ${log_path}/${table}.log                  # 将非字符串空值写成\N
 
 if [[ $? -eq 0 ]] ; then
      echo ${CURRENT} >> ${watch_path/$table}
@@ -49,31 +51,14 @@ else
      exit 1
 fi
 
-# 参数说明
-sqoop import
---connect jdbc:mysql://${ip}:${port}/${mysql_db}?tinyInt1isBit=false
---username ${username}
---password ${password}
---table ${table}
---fields-terminated-by '\001'                               			    # 设置分隔符
---where "create_time=${YESTERDAY} or update_time=${YESTERDAY}"        # 筛选数据
---hive-drop-import-delims                                   			    # 过滤掉分隔符\n, \r, \01
---hive-import                                               	        # 导入hive,不设置就使用hive默认分隔符
---hive-table base.${table} -m 4                                       # -m表示map数量
---delete-target-dir                                                   # 删除已存在的目录
---hive-overwrite                                                      # 覆盖数据
---hive-partition-key dt                                               # 分区字段dt
---hive-partition-value ${YESTERDAY}                                   # 分区值
-# hive使用'\N'存储null值,mysql就是null,为了保证导入导出数据的一致性,导入时采用--null-string和--null-non-string参数,导出时采用--input-null-string和--input-null-non-string参数
---null-string '\\N'                                        			      # 将字符串空值写成\\N
---null-non-string '\\N' &>> ${log_path}/${table}.log                  # 将非字符串空值写成\\N
 
-# sqoop增量导入有两种方式
-# 1.基于递增列的增量导入(Append方式)
+
+# sqoop增量导入两种方式
+# 基于递增列(Append方式)
 --incremental append
 --check-column id
 --last-value 0  # 每次执行job后,sqoop会自动记录该last-value值,下次执行时无需手动更改
-# 2.基于时间列的增量导入(LastModified方式)
+# 基于时间列(LastModified方式)
 --incremental lastmodified
 --check-column update_time
 --last-value "2019-05-14 15:17:23"  # 时间戳可以通过参数配置
