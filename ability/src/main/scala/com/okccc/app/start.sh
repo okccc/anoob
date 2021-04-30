968d5bc10c7475a3196c323134babe44f6583cc7
okccc <1573976179@qq.com>
2021-04-30 14:46:36 +0800

    ...

--- ability/src/main/scala/com/okccc/app/start.sh
+++ ability/src/main/scala/com/okccc/app/start.sh
@@ -17,7 +17,7 @@ class=app.$className
 yarn application --list | grep $class | awk '{system(" yarn application --kill "$1)}'
 
 # 将配置信息放到工程外面的独立文件,这样代码可以复用,可以往dev/test等不同环境写数据,只需更改配置文件不用重新编译代码
-nohup /opt/spark/bin/spark-submit \
+nohup /opt/com.okccc.spark/bin/com.okccc.spark-submit \
 --class $class \
 --files /data1/projects-app/MongoStream/$project/conf/config.properties,\
 /data1/projects-app/MongoStream/$project/conf/log4j.properties,\
@@ -33,8 +33,8 @@ nohup /opt/spark/bin/spark-submit \
 --conf "spark.yarn.max.executor.failures=3" \
 --conf "spark.executor.instances=3" \
 --conf "spark.yarn.executor.memoryOverhead=1024" \
---conf spark.eventLog.enabled=true \
---conf spark.eventLog.dir=hdfs://jiliguala/sparklogs \
+--conf com.okccc.spark.eventLog.enabled=true \
+--conf com.okccc.spark.eventLog.dir=hdfs://jiliguala/sparklogs \
 --conf "spark.executor.extraJavaOptions=-Dlog4j.configuration=log4j.properties -XX:+UseG1GC -verbose:gc -XX:+PrintGCDetails -XX:+PrintGCTimeStamps" \
 --conf "spark.streaming.kafka.maxRatePerPartition=1000000" \
 /data1/projects-app/MongoStream/$project/lib/$project-1.0-SNAPSHOT.jar 10 > /data1/projects-app/MongoStream/$project/log/$project.log &
