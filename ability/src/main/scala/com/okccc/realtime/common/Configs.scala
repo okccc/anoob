459c9dd48a9ec476fb57b2afe6ef4d70c75c62bc
okccc <1573976179@qq.com>
2021-06-27 18:28:29 +0800

    ...

--- ability/src/main/scala/com/okccc/realtime/common/Configs.scala
+++ ability/src/main/scala/com/okccc/realtime/common/Configs.scala
@@ -7,7 +7,7 @@ import java.util.Properties
  * Date: 2021/6/15 上午9:37
  * Desc: 项目中用到的配置常量
  */
-object Config {
+object Configs {
 
   // jdbc
   val JDBC_DRIVER: String = "jdbc.driver"
@@ -29,19 +29,23 @@ object Config {
   val ES_SERVER: String = "es.server"
 
   // hbase
-  val HBASE_DRIVER: String = "hbase.driver"
-  val HBASE_URL: String = "hbase.url"
+//  val HBASE_DRIVER: String = "hbase.driver"
+//  val HBASE_URL: String = "hbase.url"
 
   // kafka
   val BOOTSTRAP_SERVERS: String = "bootstrap.servers"
-  val NGINX_TOPIC: String = "nginx.topic"
-  val MYSQL_TOPIC: String = "mysql.topic"
+  val NGINX_TOPICS: String = "nginx.topics"
+  val MYSQL_TOPICS: String = "mysql.topics"
   val GROUP_ID: String = "group.id"
   val RESTART: String = "restart"
   val ACK: String = "ack"
   val IDEMPOTENCE: String = "idempotence"
   val ENABLE_AUTO_COMMIT: String = "enable.auto.commit"
   val AUTO_OFFSET_RESET: String = "auto.offset.reset"
+
+  // hive
+  val HIVE_TABLE: String = "nginx.hive.table"
+  val HIVE_COLUMNS: String = "nginx.hive.columns"
 
   // 加载配置文件
   private val prop: Properties = new Properties()
