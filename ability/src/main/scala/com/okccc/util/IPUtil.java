f52a3905bb16d554a23bc415130890cc10916d5c
okccc <1573976179@qq.com>
2021-06-28 18:32:36 +0800

    使用阿里云地理信息库解析IP地址

--- ability/src/main/scala/com/okccc/util/IPUtil.java
+++ ability/src/main/scala/com/okccc/util/IPUtil.java
@@ -1,10 +1,13 @@
 package com.okccc.util;
 
-import net.ipip.ipdb.City;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.sec.client.FastIPGeoClient;
+import com.alibaba.sec.domain.FastGeoConf;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 /**
  * Author: okccc
@@ -16,23 +19,56 @@ public class IPUtil {
     // 使用指定类初始化日志对象,控制台输出日志时会显示日志信息所属的类
     private static final Logger logger = LoggerFactory.getLogger(IPUtil.class);
 
-    private static City city;
+    // 简单解析IP地址
+//    private static City city;
+//    public static String[] find(String ip, String language) {
+//        try {
+//            if (city == null) {
+//                city = new City(IPUtil.class.getResource("/").getPath() + "ipipfree.ipdb");
+//            }
+//            return city.find(ip, language);
+//        } catch (Exception e) {
+////            e.printStackTrace();
+//            logger.error("parse ip failed: " + e);
+//        }
+//        return null;
+//    }
 
-    public static String[] find(String ip, String language) {
+    // 使用阿里云IP库解析
+    private static final String DATA_FILE_PATH = "ability/src/main/resources/geoip.dex";
+    private static final String LICENSE_FILE_PATH = "ability/src/main/resources/geoip.lic";
+    public static String getCity(String ip) throws Exception {
+        // 创建地理配置信息
+        FastGeoConf geoConf = new FastGeoConf();
+        geoConf.setDataFilePath(DATA_FILE_PATH);
+        geoConf.setLicenseFilePath(LICENSE_FILE_PATH);
+        // 指定只返回需要的字段节约内存,默认是返回全部字段"country", "province", "province_code", "city", "city_code",
+        // "county", "county_code","isp", "isp_code", "routes","longitude", "latitude"
+        HashSet<String> hashSet = new HashSet<>(Arrays.asList("province", "city", "isp"));
+        geoConf.setProperties(hashSet);
+        geoConf.filterEmptyValue();  // 过滤空值字段
+        // 通过单例模式创建对象
+        FastIPGeoClient fastIPGeoClient = FastIPGeoClient.getSingleton(geoConf);
+        // 查询ip
+        String result;
         try {
-            if (city == null) {
-                city = new City(IPUtil.class.getResource("/").getPath() + "ipipfree.ipdb");
-            }
-            return city.find(ip, language);
+            result = fastIPGeoClient.search(ip);
         } catch (Exception e) {
-//            e.printStackTrace();
-            logger.error("parse ip failed: " + e);
+            logger.error("error", e);
+            result = null;
         }
-        return null;
+        return result;
     }
 
-    public static void main(String[] args) {
-        String[] cns = find("49.83.75.64", "CN");
-        System.out.println(Arrays.toString(cns));  // [中国, 江苏, 盐城]
+
+    public static void main(String[] args) throws Exception {
+//        String[] cns = find("49.83.75.64", "CN");
+//        System.out.println(Arrays.toString(cns));  // [中国, 江苏, 盐城]
+
+        String res = getCity("49.83.75.64");
+        String province = JSON.parseObject(res).getString("province");
+        String city = JSON.parseObject(res).getString("city");
+        String isp = JSON.parseObject(res).getString("isp");
+        System.out.println(province + "," + city + "," + isp);  // 江苏省,盐城市,中国电信
     }
 }
