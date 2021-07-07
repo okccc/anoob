134bfaf9cb58345aaf3da56297ac91cdf7b8ce5e
okccc <1573976179@qq.com>
2021-07-07 10:16:47 +0800

    scala.Predef$.ArrowAssoc(Ljava/lang/Object;)

--- ability/src/main/scala/com/okccc/realtime/ods/RealTimeEvent.scala
+++ ability/src/main/scala/com/okccc/realtime/ods/RealTimeEvent.scala
@@ -1,68 +1,89 @@
 package com.okccc.realtime.ods
 
-import com.alibaba.fastjson.{JSON, JSONObject}
+import java.util
+
+import com.alibaba.fastjson.{JSON, JSONArray, JSONObject}
 import com.okccc.realtime.common.Configs
-import com.okccc.util.{HiveUtil, KafkaConsUtil, OffsetManageUtil}
+import com.okccc.util.{DateUtil, HiveUtil, IPUtil, KafkaConsUtil, LogParseUtil, OffsetManageUtil, StringUtil}
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.kafka.common.TopicPartition
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.types.StructType
-import org.apache.spark.sql.{DataFrame, Row, SparkSession}
+import org.apache.spark.sql.{Row, SparkSession}
+import org.apache.spark.streaming.{Seconds, StreamingContext}
 import org.apache.spark.streaming.dstream.{DStream, InputDStream}
 import org.apache.spark.streaming.kafka010.{HasOffsetRanges, OffsetRange}
-import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+import scala.collection.mutable.ArrayBuffer
 
 /**
  * Author: okccc
- * Date: 2021/6/15 下午3:10
- * Desc: SparkStreaming实时读取flume采集到kafka的数据并解析写入hive
+ * Date: 2021/7/3 下午5:14
+ * Desc: SparkStreaming实时读取flume同步到kafka的日志数据并解析写入hive
  */
 object RealTimeEvent {
-
   def main(args: Array[String]): Unit = {
+
+    /**
+     * 常见错误
+     * 1.java.net.UnknownHostException: jiliguala
+     * idea本地访问远程hive要把hdfs-site.xml和hive-site.xml拷过来,不然报错
+     *
+     * 2.User okc does not have privileges for ALTERTABLE_ADDPARTS
+     * idea本地访问远程hive要设置hadoop用户,本地用户没有权限 System.setProperty("HADOOP_USER_NAME", "root")
+     * 建议本地调试代码时参数不要写死,本地代码和提交到服务器的代码应该保持一致,可以在VM options添加配置-DHADOOP_USER_NAME=hdfs
+     * main方法参数配置：Edit Configurations - Main class(主类) - Program arguments(传参) - VM options(jvm配置)
+     *
+     * 3.A master URL must be set in your configuration
+     * idea本地调试spark代码要指定master,可以在VM option添加配置-Dspark.master=local[*]
+     *
+     * 4.Truncated the string representation of a plan since it was too large.
+     * This behavior can be adjusted by setting 'spark.debug.maxToStringFields' in SparkEnv.conf.
+     * spark操作表字段数默认是25,可以设置spark.debug.maxToStringFields
+     *
+     * 5.spark-submit报错 java.lang.NoSuchMethodError: scala.Predef$.ArrowAssoc(Ljava/lang/Object;)Ljava/lang/Object
+     * 这种错一般有两个原因,一个是真的缺jar包,二个是jar包冲突了(常见)
+     * idea的scala插件,pom.xml的scala及spark/flink依赖引用的scala,服务器最终执行jar包的spark/flink命令引用的scala,三者要保持版本一致
+     */
+
     // 参数校验
     if (args.length != 1) {
       println("Usage: Please input batchDuration(s)")
       System.exit(1)
     }
 
-    // idea本地访问远程hive要设置hadoop用户,本地用户没有权限
-//    System.setProperty("HADOOP_USER_NAME", "root")
-    // 本地调试代码时参数不要写死,本地代码和提交到服务器的代码应该保持一致,建议在VM options添加配置
-    // main方法参数配置：Edit Configurations - Main class(主类) - Program arguments(传参) - VM options(jvm配置)
-    // idea本地调试spark代码
-    // -Dspark.master=local[*]  A master URL must be set in your configuration
-    // -DHADOOP_USER_NAME=hdfs  User okc does not have privileges for ALTERTABLE_ADDPARTS
-    // -Dspark.sql.shuffle.partitions=1
-
     // 创建SparkSession对象
     val spark: SparkSession = SparkSession
       .builder()
       .appName("kafka-hive")
       .enableHiveSupport()
+      .config("spark.debug.maxToStringFields", 100)
       .config("hive.exec.dynamic.partition", "true")
       .config("hive.exec.dynamic.partition.mode", "nonstrict")
+      // spark集成kafka会有序列化问题
+      .config("spark.serializer", "org.apache.spark.serializer.KryoSerializer")
       .getOrCreate()
     // 获取SparkContext
     val sc: SparkContext = spark.sparkContext
     // 创建StreamingContext对象
     val ssc: StreamingContext = new StreamingContext(sc, Seconds(args(0).toLong))
     // 设置日志级别
-    sc.setLogLevel("warn")
+    sc.setLogLevel("WARN")
 
     // 看看能不能访问到hive
-    spark.sql("show databases").show()
+//    spark.sql("show databases").show()
 
     // topic和groupId
     val topics: String = Configs.get(Configs.NGINX_TOPICS)
     val groupId: String = Configs.get(Configs.GROUP_ID)
     // hive表
     val table: String = Configs.get(Configs.HIVE_TABLE)
-    val columns: String = Configs.get(Configs.HIVE_COLUMNS)
+    val common: String = Configs.get(Configs.HIVE_COLUMNS_COMMON)
+    val body: String = Configs.get(Configs.HIVE_COLUMNS_BODY)
+    val e: String = Configs.get(Configs.HIVE_COLUMNS_E)
 
     // ==================== 功能1.SparkStreaming读取kafka数据 ====================
-    // 1.从redis读取偏移量
+    // 1.每次启动时从redis读取偏移量
     val offsetMap: Map[TopicPartition, Long] = OffsetManageUtil.getOffset(topics, groupId)
     println(offsetMap)
 
@@ -81,67 +102,10 @@ object RealTimeEvent {
       rdd
     })
 
-    // 4.转换DStream数据结构
-    val RowDStream: DStream[Row] = offsetDStream.map((record: ConsumerRecord[String, String]) => {
-      // 获取ConsumerRecord的value部分
-      val jsonStr: String = record.value()
-      // 将json字符串转换成json对象
-      val jsonObj: JSONObject = JSON.parseObject(jsonStr)
-      // 先解析公共字段
-      val ts: String = jsonObj.getString("@timestamp")
-      val hostname: String = jsonObj.getString("hostname")
-      val remote_addr: String = jsonObj.getString("remote_addr")
-      val ip: String = jsonObj.getString("ip")
-      val method: String = jsonObj.getString("Method")
-      val referer: String = jsonObj.getString("referer")
-      val request: String = jsonObj.getString("request")
-      val status: String = jsonObj.getString("status")
-      val bytes: String = jsonObj.getString("bytes")
-      val agent: String = jsonObj.getString("agent")
-      val x_forwarded: String = jsonObj.getString("x_forwarded")
-      // 再解析body字段
-      val body: String = jsonObj.getString("request_body")
-      val p_date: String = "20210623"
-      // 封装成Row对象
-      Row(ts, hostname, remote_addr, ip, method, referer, request, body, status, bytes, agent, x_forwarded, p_date)
-    })
-
-    RowDStream.foreachRDD((rdd: RDD[Row]) => {
-      // 根据列创建hive表结构
-      val schema: StructType = HiveUtil.createSchema(columns)
-      // 将RDD转换为DataFrame
-      val df: DataFrame = spark.createDataFrame(rdd, schema)
-      // 生成临时表
-      df.repartition(30).createOrReplaceTempView("event")
-      // 将临时表数据插入正式表
-      spark.sql("insert into table " + table + " partition(p_date) select * from event")
-      // 处理完当前批次数据要更新偏移量,由于消费速度低于生产速度导致消息积压,虽然每隔5秒获取一批数据但是更新offset时可能已经过去了好几个5秒
-      // 所以输出的更新分区偏移量不是连续的,更新分区偏移量: 0 923~971,更新分区偏移量: 0 1554~1602,更新分区偏移量: 0 2184~2232
-      OffsetManageUtil.updateOffset(topics, groupId, offsetRanges)
-    })
 
 
-    // 4.输出操作
-    //    offsetDStream.foreachRDD((rdd: RDD[ConsumerRecord[String, String]]) => {
-    //      // 以分区为单位处理数据
-    //      rdd.mapPartitions((iterator: Iterator[ConsumerRecord[String, String]]) => {
-    //        // 结构转换
-    //        iterator.map((record: ConsumerRecord[String, String]) => {
-    //          // 获取ConsumerRecord的value部分
-    //          val jsonStr: String = record.value()
-    //          // 将json字符串封装成json对象
-    //          val jsonObj: JSONObject = JSON.parseObject(jsonStr)
-    //          // 解析json对象
-    //          val body: String = jsonObj.getString("request_body")
-    //          // url解码
-    //          body
-    //        })
-    //      })
-    //    })
-
-    // 启动程序
+    // 6.启动程序
     ssc.start()
     ssc.awaitTermination()
   }
-
 }
