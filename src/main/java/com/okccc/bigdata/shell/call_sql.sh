a90a6756380b6aba0f3b1473896b676e582f0b4b
okccc <1573976179@qq.com>
2021-07-23 18:08:50 +0800

    ...

--- src/main/java/com/okccc/bigdata/shell/call_sql.sh
+++ src/main/java/com/okccc/bigdata/shell/call_sql.sh
@@ -19,13 +19,13 @@ hqlpath=/home/hive/gds/hql
 ERRORLOG=${logpath}/${sqlfile}_${stat_date}.log
 
 # hive shell -> sql对应的dt=${hivevar:dt}
-hive -hivevar dt=${stat_date} -f ${hqlpath}/${sqlfile}.sql &>> ${ERRORLOG}
+hive -hivevar dt="${stat_date}" -f ${hqlpath}/"${sqlfile}".sql &>> "${ERRORLOG}"
 # impala-shell -> sql对应的dt='${var:dt}'
-impala-shell -i master2 --var=dt=${stat_date} -f ${hqlpath}/${sqlfile}.sql &>> ${ERRORLOG}
+impala-shell -i master2 --var=dt="${stat_date}" -f ${hqlpath}/"${sqlfile}".sql &>> "${ERRORLOG}"
 
 if [ $? -eq 0 ];then
-    echo "---call proc succeed at $CURRENT---" >> ${ERRORLOG}
+    echo "---call proc succeed at $CURRENT---" >> "${ERRORLOG}"
 else
-    echo "---call proc failed at $CURRENT---" >>  ${ERRORLOG}
+    echo "---call proc failed at $CURRENT---" >>  "${ERRORLOG}"
     exit 1
 fi
