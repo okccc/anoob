b9a3e7dd5fd411c85a476f4e9a46074964914bcd
okccc <1573976179@qq.com>
2021-07-31 10:32:42 +0800

    解析binlog的工具类

--- src/main/scala/com/okccc/util/CanalUtil.scala
+++ src/main/scala/com/okccc/util/CanalUtil.scala
@@ -14,12 +14,41 @@ import scala.collection.mutable.ArrayBuffer
  */
 object CanalUtil {
 
+  def parseJSONObjectToArrayBuffer(jsonObj: JSONObject, hiveTable: String): ArrayBuffer[ArrayBuffer[String]] = {
+    // 存放所有记录的可变数组
+    val records: ArrayBuffer[ArrayBuffer[String]] = new ArrayBuffer[ArrayBuffer[String]]()
+    // 获取表名/操作记录/操作类型/操作时间
+    val db: String = jsonObj.getString("database")
+    val table: String = jsonObj.getString("table")
+    val data: JSONArray = jsonObj.getJSONArray("data")
+    val operation: String = jsonObj.getString("type")
+    val ts: String = jsonObj.getString("ts")
+    val dt: String = DateUtil.parseUnixToDate(ts.toLong).replace("-", "")
+    if (table == hiveTable.split("\\.")(1)) {
+      // 解析json数组
+      for (i <- 0 until data.size()) {
+        // json数组包含多个小json串
+        val jsonObj: JSONObject = JSON.parseObject(data.get(i).toString)
+        // 存放当前记录的字段值
+        val record: ArrayBuffer[String] = new ArrayBuffer[String]()
+        // 获取该表所有字段
+        val columns: String = Configs.get(table + ".hive.columns")
+        val arr: Array[String] = columns.split(",")
+        for (column <- util.Arrays.copyOf(arr, arr.length - 5)) {
+          // 获取每个字段对应的值
+          record.append(jsonObj.getOrDefault(column, "").toString)
+        }
+        record.append(db, table, operation, ts, dt)
+        records.append(record)
+      }
+    }
+    records
+  }
+
   def main(args: Array[String]): Unit = {
     val str: String = "tmp.orders"
     // 注意：split切割字符串 "," ":" "@" "#"不需要转义, "." "|" "$" "*"是需要转义的,多个分隔符可以用"|"隔开,但是该转义的还得转义
     val arr: Array[String] = str.split("\\.")
-    for (elem <- arr) {
-      println(elem)
-    }
+    println(arr(1))
   }
 }
