3291d7d7252fc774a7aee023c83ef70568d71ae5
okccc <1573976179@qq.com>
2021-08-10 09:31:09 +0800

    里氏替换原则

---
+++ src/main/java/com/okccc/design/princple/LiskovReplace.java
@@ -0,0 +1,45 @@
+package com.okccc.design.princple;
+
+/**
+ * Author: okccc
+ * Date: 2021/8/3 下午5:02
+ * Desc: 里式替换原则
+ */
+public class LiskovReplace {
+    public static void main(String[] args) {
+        AA aa = new AA();
+        System.out.println("10+20 = " + aa.func01(10, 20));  // 10+20 = 30
+
+        B01 b01 = new B01();
+        System.out.println("10-20 = " + b01.func01(10, 20));  // 10-20 = -10
+
+        B02 b02 = new B02();
+        System.out.println("10-20 = " + b02.func01(10, 20));  // 10-20 = -10
+        System.out.println("10+20 = " + b02.func02(10, 20));  // 10+20 = 30
+    }
+}
+
+class AA {
+    public int func01(int a, int b) {
+        return a + b;
+    }
+}
+
+// 子类继承父类时重写方法会无意中改变父类原方法的功能
+class B01 extends AA {
+    public int func01(int a, int b) {
+        return  a - b;
+    }
+}
+
+class B02 {
+    public int func01(int a, int b) {
+        return a - b;
+    }
+
+    // 如果要用到A01类的方法,使用组合关系
+    private final AA a = new AA();
+    public int func02(int a, int b) {
+        return this.a.func01(a, b);
+    }
+}
