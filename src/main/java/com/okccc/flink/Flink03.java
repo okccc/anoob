e4daa206a258c47a23d9b6e6b619f2f1ca13ea80
okccc <1573976179@qq.com>
2021-09-15 10:30:58 +0800

    flink开窗算子window

---
+++ src/main/java/com/okccc/flink/Flink03.java
@@ -0,0 +1,31 @@
+package com.okccc.flink;
+
+import org.apache.flink.api.common.functions.AggregateFunction;
+import org.apache.flink.api.common.state.MapState;
+import org.apache.flink.api.common.state.MapStateDescriptor;
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.configuration.Configuration;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.KeyedProcessFunction;
+import org.apache.flink.streaming.api.functions.windowing.ProcessWindowFunction;
+import org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows;
+import org.apache.flink.streaming.api.windowing.time.Time;
+import org.apache.flink.streaming.api.windowing.windows.TimeWindow;
+import org.apache.flink.util.Collector;
+
+import java.sql.Timestamp;
+
+/**
+ * Author: okccc
+ * Date: 2021/9/9 下午5:05
+ * Desc:
+ */
+public class Flink03 {
+    public static void main(String[] args) throws Exception {
+        /*
+         * flink也有窗口函数,但不代表这就是微批处理,flink流处理指的是数据来一条处理一条,开窗是业务需求,有些场景必须要攒一批数据
+         * keyBy()是分组后聚合,keyBy() + window()是分组并各自开窗后再聚合,分组就是将数据分到不同的流
+         *
+         * 滚动窗口：窗口大小固定,没有重叠,一个元素只会属于一个窗口,是滑动窗口的特殊情况,窗口大小和滑动间隔相等
+         * 滑动窗口：窗口大小固定,有滑动间隔所以会有重叠,一个元素可以属于多个窗口
+         * 会话窗口：前两者统计的是网站PV/UV这种固定时间的,而用户访问行为是不固定的,超时时间内没收到新数据就生成新的窗口,只有flink支持会话窗口
