27eb216e56baff8f56ec94ce9a4c640609e62235
okccc <1573976179@qq.com>
2021-10-08 15:30:54 +0800

    java工厂模式

--- src/main/java/com/okccc/design/FactoryDemo.java
+++ src/main/java/com/okccc/design/FactoryDemo.java
@@ -3,7 +3,13 @@ package com.okccc.design;
 /**
  * Author: okccc
  * Date: 2021/7/13 下午8:43
- * Desc: 
+ * Desc: java工厂模式
  */
 public class FactoryDemo {
+    public static void main(String[] args) {
+        /*
+         * 工厂方法模式：代替new创建复杂对象,定义创建对象的接口,由子类自己决定实例化哪一个工厂类
+         * 抽象工厂模式：是其它工厂的超级工厂,工厂模式创建一个对象,抽象工厂模式创建一组对象
+         */
+    }
 }
