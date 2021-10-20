c73a1db23476a7131416438abe10a39108b1c4fb
okccc <1573976179@qq.com>
2021-10-20 10:02:32 +0800

    flink-cdc自定义反序列化器

--- src/main/java/com/okccc/flink/FlinkCdc.java
+++ src/main/java/com/okccc/flink/FlinkCdc.java
@@ -1,10 +1,18 @@
 package com.okccc.flink;
 
+import com.alibaba.fastjson.JSONObject;
 import com.alibaba.ververica.cdc.connectors.mysql.MySQLSource;
 import com.alibaba.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.alibaba.ververica.cdc.debezium.DebeziumDeserializationSchema;
+import io.debezium.data.Envelope;
+import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
 import org.apache.flink.table.api.bridge.java.StreamTableEnvironment;
+import org.apache.flink.util.Collector;
+import org.apache.kafka.connect.data.Field;
+import org.apache.kafka.connect.data.Struct;
+import org.apache.kafka.connect.source.SourceRecord;
 
 /**
  * Author: okccc
@@ -49,6 +57,7 @@ public class FlinkCdc {
                 .password("root@123")
                 .startupOptions(StartupOptions.initial())  // initial启动时会扫描历史数据,然后继续读取最新的binlog
 //                .deserializer(new StringDebeziumDeserializationSchema()) // 默认反序列化方式返回的数据格式不太友好
+                .deserializer(new MyStringDeserializationSchema())
                 .build();
         env
                 .addSource(sourceFunction)
@@ -79,6 +88,52 @@ public class FlinkCdc {
         tableEnv
                 .executeSql("select * from user_info")
                 .print();
+    }
+
+    // 自定义反序列化器,封装返回的数据格式,方便解析
+    public static class MyStringDeserializationSchema implements DebeziumDeserializationSchema<String> {
+        @Override
+        public void deserialize(SourceRecord sourceRecord, Collector<String> collector) throws Exception {
+            Struct valueStruct = (Struct) sourceRecord.value();
+            Struct sourceStrut = valueStruct.getStruct("source");
+            // 获取数据库
+            String database = sourceStrut.getString("db");
+            // 获取表
+            String table = sourceStrut.getString("table");
+            // 获取类型
+            String type = Envelope.operationFor(sourceRecord).toString().toLowerCase();
+            if(type.equals("create")){
+                type="insert";
+            }
+
+            // 封装成JSON对象
+            JSONObject jsonObj = new JSONObject();
+            jsonObj.put("database",database);
+            jsonObj.put("table",table);
+            jsonObj.put("type",type);
+
+            // 获取影响的数据data
+            // 源格式：id=1, name=aaa, age=17
+            // 目标格式：{"id": 74603, "order_id": 28641, "order_status": "1005", "operate_time": "2021-07-30 11:35:49"}}
+            Struct afterStruct = valueStruct.getStruct("after");
+            JSONObject dataJsonObj = new JSONObject();
+            if (afterStruct != null){
+                for (Field field : afterStruct.schema().fields()) {
+                    String fieldName = field.name();
+                    Object fieldValue = afterStruct.get(field);
+                    dataJsonObj.put(fieldName, fieldValue);
+                }
+            }
+            jsonObj.put("data", dataJsonObj);
+
+            // 向下游传递数据
+            collector.collect(jsonObj.toJSONString());
+        }
+
+        @Override
+        public TypeInformation<String> getProducedType() {
+            return TypeInformation.of(String.class);
+        }
     }
 
 }
