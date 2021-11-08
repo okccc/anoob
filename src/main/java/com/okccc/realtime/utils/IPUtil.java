4a560cfa15493284d39cd2ca3fe3b9c830c48ee4
okccc <1573976179@qq.com>
2021-11-08 17:08:04 +0800

    ip工具类

---
+++ src/main/java/com/okccc/realtime/utils/IPUtil.java
@@ -0,0 +1,64 @@
+package com.okccc.realtime.utils;
+
+import com.alibaba.sec.client.FastIPGeoClient;
+import com.alibaba.sec.domain.FastGeoConf;
+import net.ipip.ipdb.City;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * Author: okccc
+ * Date: 2021/10/28 上午10:20
+ * Desc: 解析IP地址
+ */
+public class IPUtil {
+    
+    // 简单解析IP地址
+    private static City city = null;
+
+    public static String[] find(String ip) {
+        try {
+            if (city == null) {
+                city = new City(ClassLoader.getSystemResource("ipipfree.ipdb").getPath());
+            }
+            return city.find(ip, "CN");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    // 使用阿里云IP库解析(收费)
+    // https://help.aliyun.com/document_detail/170314.html?spm=a2c4g.11186623.6.583.6d6a1d5c159Enh
+    private static final String DATA_FILE_PATH = ClassLoader.getSystemResource("geoip.dex").getPath();
+    private static final String LICENSE_FILE_PATH = ClassLoader.getSystemResource("geoip.lic").getPath();
+
+    public static String getCity(String ip) throws Exception {
+        // 创建地理配置信息
+        FastGeoConf geoConf = new FastGeoConf();
+        geoConf.setDataFilePath(DATA_FILE_PATH);
+        geoConf.setLicenseFilePath(LICENSE_FILE_PATH);
+        // 指定需要的字段节约内存,默认是返回全部字段"country", "province", "province_code", "city", "city_code",
+        // "county", "county_code", "isp", "isp_code", "routes", "longitude", "latitude"
+        HashSet<String> hashSet = new HashSet<>(Arrays.asList("province", "city", "isp"));
+        geoConf.setProperties(hashSet);
+        geoConf.filterEmptyValue();  // 过滤空值字段
+        // 通过单例模式创建对象
+        FastIPGeoClient fastIPGeoClient = FastIPGeoClient.getSingleton(geoConf);
+        // 查询ip
+        String result = null;
+        try {
+            result = fastIPGeoClient.search(ip);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception {
+        System.out.println(Arrays.toString(find("49.83.75.64")));  // [中国, 江苏, 盐城]
+        System.out.println(getCity("49.83.75.64"));  // {"province":"江苏省","city":"盐城市","isp":"中国电信"}
+    }
+
+}
