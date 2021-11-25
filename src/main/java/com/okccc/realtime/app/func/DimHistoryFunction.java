882b9359ff27a8a4346bc3d1277c950fa4b591dd
okccc <1573976179@qq.com>
2021-11-25 10:36:07 +0800

    flink-cdc将mysql维度表历史数据同步到hbase

---
+++ src/main/java/com/okccc/realtime/app/func/DimHistoryFunction.java
@@ -0,0 +1,100 @@
+package com.okccc.realtime.app.func;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.ververica.cdc.connectors.mysql.MySQLSource;
+import com.alibaba.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.alibaba.ververica.cdc.debezium.DebeziumSourceFunction;
+import com.okccc.realtime.utils.DimUtil;
+import org.apache.flink.api.common.functions.MapFunction;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Author: okccc
+ * Date: 2021/11/17 下午3:03
+ * Desc: canal/maxwell是基于binlog抓最新数据,mysql中维度表的历史数据要先通过flink-cdc手动同步到hbase
+ */
+public class DimHistoryFunction {
+    public static void main(String[] args) throws Exception {
+        // 创建流处理执行环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        // 获取数据源
+        DebeziumSourceFunction<String> sourceFunction = MySQLSource.<String>builder()
+                .hostname("localhost")
+                .port(3306)
+                .databaseList("maxwell")
+                // 需要同步历史数据的维度表
+                .tableList("maxwell.user_info,maxwell.base_province,maxwell.sku_info,maxwell.spu_info,maxwell.base_trademark,maxwell.base_category3")
+                .username("root")
+                .password("root@123")
+                .startupOptions(StartupOptions.initial())
+                .deserializer(new MyDeserialization())
+                .build();
+        DataStreamSource<String> dataStream = env.addSource(sourceFunction);
+
+        // 结构转换
+        SingleOutputStreamOperator<JSONObject> jsonStream = dataStream.map(new MapFunction<String, JSONObject>() {
+            @Override
+            public JSONObject map(String value) {
+                // {"database":"maxwell", "data":{"gender":"F","name":"moon"...}, "type":"insert", "table":"user_info"}
+                return JSON.parseObject(value);
+            }
+        });
+
+        // 写入hbase
+        jsonStream.addSink(new RichSinkFunction<JSONObject>() {
+            @Override
+            public void invoke(JSONObject value, Context context) {
+                /* {
+                 *     "data":{
+                 *         "birthday":11660,
+                 *         "gender":"F",
+                 *         "create_time":1607124346000,
+                 *         "login_name":"king",
+                 *         "name":"moon",
+                 *         "user_level":"1",
+                 *         "phone_num":"13528938229",
+                 *         "id":3773,
+                 *         "email":"xxx@gmall.com",
+                 *         "operate_time":1607124525000
+                 *     },
+                 *     "type":"insert",
+                 *     "database":"maxwell",
+                 *     "table":"user_info"
+                 * }
+                 */
+
+                // 获取hbase表名
+                String tableName = "dim_" + value.getString("table");
+                // 获取数据
+                JSONObject data = value.getJSONObject("data");
+                // 部分表要先过滤不需要的字段(可选)
+                if ("dim_user_info".equals(tableName)) {
+                    String column = "id,login_name,name,user_level,birthday,gender,create_time,operate_time";
+                    List<String> columns = Arrays.asList(column.split(","));
+                    Set<String> keys = data.keySet();
+                    keys.removeIf(s -> !columns.contains(s));
+                } else if ("dim_base_trademark".equals(tableName)) {
+                    String column = "id,tm_name";
+                    List<String> columns = Arrays.asList(column.split(","));
+                    Set<String> keys = data.keySet();
+                    keys.removeIf(s -> !columns.contains(s));
+                }
+                // 根据表名和data插入数据
+                DimUtil.updateDimInfo(tableName, data);
+            }
+        });
+
+        // 启动任务
+        env.execute();
+    }
+}
--- src/main/java/com/okccc/realtime/utils/DimUtil.java
+++ src/main/java/com/okccc/realtime/utils/DimUtil.java
@@ -2,6 +2,7 @@ package com.okccc.realtime.utils;
 
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.flink.api.java.tuple.Tuple2;
 import redis.clients.jedis.Jedis;
 
@@ -10,11 +11,13 @@ import java.util.List;
 /**
  * Author: okccc
  * Date: 2021/11/2 上午11:00
- * Desc: 基于phoenix查询维度数据的工具类
+ * Desc: 基于phoenix读写hbase维度数据的工具类
  */
 public class DimUtil {
 
-    // 从phoenix表查询维度数据,参数1是表名,参数2是查询条件即列和值,因为条件个数未知所以选用不定长参数的Tuple2
+    /**
+     * 将phoenix查询数据方法继续封装,通常不会直接传入sql字符串,参数1是表名,参数2是查询条件即列和值,因为条件个数未知所以选用不定长参数的Tuple2
+     */
     @SafeVarargs
     public static JSONObject getDimInfo(String tableName, Tuple2<String, String>... columnAndValues) {
         // 拼接查询sql
@@ -43,7 +46,9 @@ public class DimUtil {
         return jsonObject;
     }
 
-    // 使用旁路缓存优化,提升维度查询效率
+    /**
+     * redis旁路缓存优化,提升维度查询效率
+     */
     @SafeVarargs
     public static JSONObject getDimInfoWithCache(String tableName, Tuple2<String, String>... columnAndValues) {
         // 拼接查询sql
@@ -103,14 +108,23 @@ public class DimUtil {
         // 用完及时释放连接,不然连接池很快就消耗完
         if (jedis != null) {
             jedis.close();
-            System.out.println("---关闭redis连接---");
         }
 
         // 返回最终查询结果
         return jsonObject;
     }
 
-    // 删除redis缓存数据
+    /**
+     * 默认按照主键id查询,即只有一个查询条件
+     */
+    public static JSONObject getDimInfoWithCache(String tableName, String id) {
+        // 将id封装成Tuple2
+        return getDimInfoWithCache(tableName, Tuple2.of("id", id));
+    }
+
+    /**
+     * 删除redis缓存数据
+     */
     public static void deleteCache(String tableName, String id) {
         String key = tableName.toLowerCase() + ":" + id;
         try {
@@ -123,11 +137,23 @@ public class DimUtil {
         }
     }
 
-    public static void main(String[] args) {
-//        JSONObject dimInfo = getDimInfo("dim_base_trademark", Tuple2.of("id", "12"));
-//        System.out.println(dimInfo);  // {"ID":"12","TM_NAME":"联想"}
-        JSONObject dimInfoWithCache = getDimInfoWithCache("dim_base_trademark", Tuple2.of("id", "12"));
-        System.out.println(dimInfoWithCache);
+    /**
+     * 将phoenix更新数据方法继续封装,通常不会直接传入sql字符串,参数1是表名,参数2和参数3是更新的字段和值
+     */
+    public static void updateDimInfo(String tableName, JSONObject data) {
+        // 获取插入表的字段和对应值
+        String fields = StringUtils.join(data.keySet(), ",");
+        String values = StringUtils.join(data.values(), "','");  // value之间的逗号要加引号,不然传入的是一整个字符串
+        // 拼接sql
+        String dimSql = "upsert into " + tableName + " (" + fields + ") " + "values('" + values + "')";
+//        System.out.println("dimSql =  " + sql);  // dimSql = upsert into base_trademark (id,tm_name) values("10","小米")
+        // 更新插入数据
+        PhoenixUtil.upsert(dimSql);
     }
 
+    public static void main(String[] args) {
+//        System.out.println(getDimInfo("dim_base_trademark", Tuple2.of("id", "12")));
+//        System.out.println(getDimInfoWithCache("dim_base_trademark", Tuple2.of("id", "12")));
+        System.out.println(getDimInfoWithCache("dim_base_trademark", "12"));  // {"ID":"12","TM_NAME":"联想"}
+    }
 }
