e05d3c9747505d21c15c941fbc4fbce7811b46a4
okccc <1573976179@qq.com>
2021-12-24 10:33:50 +0800

    es/spark/flink都大量使用了构造者模式

--- src/main/java/com/okccc/design/BuilderDemo.java
+++ src/main/java/com/okccc/design/BuilderDemo.java
@@ -13,13 +13,14 @@ public class BuilderDemo {
          * 2.Builder类的属性和外部类保持一致,由Builder类实现属性的setXxx方法,并最终提供build方法返回外部类对象
          * 优点：通过Builder类一步一步构建复杂对象,可以任意组合输入参数,避免多参数构造方法重载出错,还不用写过多的构造器
          *
-         * es/spark/flink都使用了构造者模式
+         * es/spark/flink都大量使用了构造者模式
          * io.searchbox.client.config.HttpClientConfig
          * io.searchbox.core.Index/Get/Update/Delete/Search
          * org.apache.spark.sql.SparkSession
-         * org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink
+         * org.apache.flink.connector.jdbc.JdbcExecutionOptions/JdbcConnectionOptions
          * org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig
          * org.apache.flink.streaming.connectors.elasticsearch6.ElasticsearchSink
+         * org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink
          */
         User user = new User.Builder().setName("fly").setMobile("111").setEmail("orc@qq.com").build();
         System.out.println(user.getName());
@@ -36,7 +37,7 @@ public class BuilderDemo {
             this.address = address;
         }
         // 包含name和email的构造方法
-        // 当构造方法的参数个数和参数类型完全一样时,方法重载会报错：User01(String, String) is already defined in com.okccc.design.User
+        // 当构造方法的参数个数和参数类型完全一样时,方法重载会报错：'User01(String, String)' is already defined
 //        public User01(String name, String email) {
 //            this.name = name;
 //            this.address = address;
