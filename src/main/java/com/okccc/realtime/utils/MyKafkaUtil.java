a8f6cb0046eafc6f2bf4d01975d6b10adba9474d
okccc <1573976179@qq.com>
2021-12-27 17:04:57 +0800

    flink从kafka读数据的消费者

--- src/main/java/com/okccc/realtime/utils/MyKafkaUtil.java
+++ src/main/java/com/okccc/realtime/utils/MyKafkaUtil.java
@@ -2,7 +2,6 @@ package com.okccc.realtime.utils;
 
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 
 import java.util.Properties;
@@ -10,21 +9,21 @@ import java.util.Properties;
 /**
  * Author: okccc
  * Date: 2021/10/4 下午7:41
- * Desc: 操作kafka的工具类
+ * Desc: flink读写kafka的工具类
  */
 public class MyKafkaUtil {
     private static final String KAFKA_SERVER = "localhost:9092";
+    private static final String DEFAULT_TOPIC = "default";
 
-    // 获取kafka的消费者
+    /**
+     * 从kafka读数据的消费者
+     */
     public static FlinkKafkaConsumer<String> getKafkaSource(String topic, String groupId) {
+        // 消费者属性配置
         Properties props = new Properties();
         props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
         props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        // 创建flink消费者对象
         return new FlinkKafkaConsumer<>(topic, new SimpleStringSchema(), props);
-    }
-
-    // 获取kafka的生产者
-    public static FlinkKafkaProducer<String> getKafkaSink(String topic) {
-        return new FlinkKafkaProducer<>(KAFKA_SERVER, topic, new SimpleStringSchema());
     }
 }
