a04a49bc3e5c6c91204472e76cff9670140c0649
okccc <1573976179@qq.com>
2022-01-29 15:52:12 +0800

    读取配置文件的工具类

---
+++ src/main/java/com/okccc/realtime/utils/PropertiesUtil.java
@@ -0,0 +1,31 @@
+package com.okccc.realtime.utils;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Properties;
+
+/**
+ * Author: okccc
+ * Date: 2022/1/29 3:36 下午
+ * Desc: 读取配置文件的工具类
+ */
+public class PropertiesUtil {
+
+    public static Properties load(String fileName) {
+        // 创建Properties对象
+        Properties prop = new Properties();
+        // 加载resource配置文件
+        try {
+            prop.load(ClassLoader.getSystemClassLoader().getResourceAsStream(fileName));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return prop;
+    }
+
+    public static void main(String[] args) {
+        Properties prop = PropertiesUtil.load("config.properties");
+        System.out.println(prop.getProperty("bootstrap.servers"));
+    }
+}
