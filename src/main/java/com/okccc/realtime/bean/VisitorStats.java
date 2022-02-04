6dc9bc4d956564b667b0321367eb88ff67e88f26
okccc <1573976179@qq.com>
2022-02-04 10:51:27 +0800

    ...

--- src/main/java/com/okccc/realtime/bean/VisitorStats.java
+++ src/main/java/com/okccc/realtime/bean/VisitorStats.java
@@ -11,19 +11,18 @@ import lombok.Data;
 @Data
 @AllArgsConstructor
 public class VisitorStats {
-    // 开始时间
+    // 窗口区间(开窗)
     private String stt;
-    // 结束时间
     private String edt;
 
-    // 维度数据
+    // 维度数据(分组)
     private String vc;  // 版本
     private String ch;  // 渠道
     private String ar;  // 地区
     private String is_new;  // 新老用户标识
 
-    // 度量数据
-    private Long pv_ct;  // 页面访问数
+    // 度量数据(聚合)
+    private Long pv_ct;  // 页面访问数  
     private Long uv_ct;  // 独立访客数
     private Long sv_ct;  // 进入页面次数
     private Long uj_ct;  // 跳出页面次数
