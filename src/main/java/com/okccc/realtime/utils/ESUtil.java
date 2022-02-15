ef24e9f029acbdae2fd3ed478345cd4f0efc010d
okccc <1573976179@qq.com>
2022-02-15 16:43:14 +0800

    es工具类

--- src/main/java/com/okccc/realtime/utils/ESUtil.java
+++ src/main/java/com/okccc/realtime/utils/ESUtil.java
@@ -1,242 +1,272 @@
 package com.okccc.realtime.utils;
 
-import com.okccc.realtime.common.MyConfig;
-import io.searchbox.client.JestClient;
-import io.searchbox.client.JestClientFactory;
-import io.searchbox.client.config.HttpClientConfig;
-import io.searchbox.core.*;
+import com.alibaba.fastjson.JSON;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.client.indices.GetIndexRequest;
+import org.elasticsearch.client.indices.GetIndexResponse;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.MatchQueryBuilder;
-import org.elasticsearch.index.query.TermQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.MaxAggregationBuilder;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
-import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
-import org.elasticsearch.search.sort.SortOrder;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Author: okccc
- * Date: 2021/10/27 下午2:41
+ * Date: 2022/2/11 10:38 上午
  * Desc: es工具类
  */
 public class ESUtil {
 
-    // 声明jest客户端工厂
-    private static JestClientFactory jestClientFactory = null;
+    private static final RestHighLevelClient restClient;
 
-    // 从jest客户端工厂获取jest客户端
-    public static JestClient getJestClient() throws IOException {
-        if (jestClientFactory == null) {
-            jestClientFactory = build();
-        }
-        return jestClientFactory.getObject();
-    }
-
-    public static JestClientFactory build() throws IOException {
-        // 创建jest客户端工厂
-        JestClientFactory factory = new JestClientFactory();
-        // 工厂属性配置,HttpClientConfig使用了构造者模式
-        factory.setHttpClientConfig(new HttpClientConfig
-                .Builder(MyConfig.ES_SERVER)
-                .maxTotalConnection(10)
-                .connTimeout(10000)
-                .readTimeout(1000)
-                .multiThreaded(true)
-                .build()
-        );
-        return factory;
-    }
-
-    // 往index中插入单个document
-    public static void putIndex(String indexName) throws IOException {
-        // 获取jest客户端
-        JestClient jestClient = getJestClient();
-        // 封装Index对象,将样例类对象作为Builder的参数传入,底层会将其转换为json字符串,id可以不写会随机生成文档id
-        Index index = new Index.Builder(generateSource()).index(indexName).type("_doc").id("1").build();
-        // 执行index插入操作,java代码此处需要try catch不然会编译异常,scala没有所谓的编译异常所以不需要
-        try {
-            // 查看源码发现execute方法需传入Action<T>接口,F4查找发现有Index/Get/Update/Delete等实现类,并且这些类都使用了Builder模式
-            jestClient.execute(index);
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // 关闭连接
-            jestClient.close();
-        }
+    // 创建es客户端
+    static {
+        // 不要密码
+        restClient = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")));
+        // 需要密码
+//        BasicCredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+//        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials("readonly", "readonly"));
+//        restClient = new RestHighLevelClient(
+//                RestClient
+//                        .builder(new HttpHost("192.168.152.11", 9200, "http"))
+//                        .setHttpClientConfigCallback(new RestClientBuilder.HttpClientConfigCallback() {
+//                            @Override
+//                            public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpClientBuilder) {
+//                                return httpClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+//                            }
+//                        })
+//        );
     }
 
     /**
-     * 往index中批量插入document
-     * @param indexName 索引名称
-     * @param docs Map<doc_id, doc>文档集合,doc_id是文档编号保证数据的幂等性,doc是具体文档通常对应java bean
+     * 索引操作
      */
-    public static void bulkIndex(String indexName, Map<String, Object> docs) throws IOException {
-        if(docs != null && docs.size() > 0) {
-            // 获取jest客户端
-            JestClient jestClient = getJestClient();
-            // 封装Bulk对象
-            Bulk.Builder builder = new Bulk.Builder();
-            // 遍历文档列表
-            for (Map.Entry<String, Object> entry : docs.entrySet()) {
-                // 每个doc都是Index对象
-                Index index = new Index.Builder(entry.getValue()).index(indexName).type("_doc").id(entry.getKey()).build();
-                // 将Index对象添加到Bulk对象中
-                builder.addAction(index);
-            }
-            Bulk bulk = builder.build();
+    public static void doIndex() throws IOException {
+        // 创建索引
+        CreateIndexRequest createRequest = new CreateIndexRequest("user");
+        CreateIndexResponse createResponse = restClient.indices().create(createRequest, RequestOptions.DEFAULT);
+        System.out.println(createResponse.isAcknowledged());
 
-            try {
-                // 执行bulk批量操作
-                BulkResult result = jestClient.execute(bulk);
-                List<BulkResult.BulkResultItem> items = result.getItems();
-                System.out.println("往es的 " + indexName + " 索引中插入了 " + items.size() + " 条文档");
-            } catch (Exception e) {
-                e.printStackTrace();
-            } finally {
-                // 关闭连接
-                jestClient.close();
-            }
+        // 查询索引
+        GetIndexRequest getRequest = new GetIndexRequest("user");
+        GetIndexResponse getResponse = restClient.indices().get(getRequest, RequestOptions.DEFAULT);
+        System.out.println(getResponse.getAliases());
+        System.out.println(getResponse.getMappings());
+        System.out.println(getResponse.getSettings());
+
+        // 删除索引
+        DeleteIndexRequest deleteRequest = new DeleteIndexRequest("user");
+        AcknowledgedResponse deleteResponse = restClient.indices().delete(deleteRequest, RequestOptions.DEFAULT);
+        System.out.println(deleteResponse.isAcknowledged());
+
+        // 关闭es客户端
+        restClient.close();
+    }
+
+    /**
+     * 插入文档
+     */
+    public static void insertDoc() throws IOException {
+        // 创建IndexRequest对象
+        IndexRequest indexRequest = new IndexRequest();
+        // 指定_index和_id,索引会自动创建,不写id会自动生成uuid
+        indexRequest.index("user").id("003");
+        // 生成POJO类对象并将其转换成json字符串
+        User user = new User("grubby", "男", 18);
+        // json常用解析库：jackson(SpringMVC)、fastjson(Alibaba)、gson(Google)
+        String jsonStr = JSON.toJSONString(user);
+        // 添加_source,必须是json格式,不然报错The number of object passed must be even but was [1],是因为没传第二个参数
+        indexRequest.source(jsonStr, XContentType.JSON);
+        // es客户端执行index操作
+        IndexResponse indexResponse = restClient.index(indexRequest, RequestOptions.DEFAULT);
+        System.out.println(indexResponse.getResult());  // CREATED/UPDATED
+        // 关闭es客户端
+        restClient.close();
+    }
+
+    /**
+     * 修改文档
+     */
+    public static void updateDoc() throws IOException {
+        // 创建UpdateRequest对象
+        UpdateRequest updateRequest = new UpdateRequest();
+        // 指定_index和_id
+        updateRequest.index("user").id("01");
+        // 修改doc
+        updateRequest.doc(XContentType.JSON, "age", 20);
+        // es客户端执行update操作
+        UpdateResponse updateResponse = restClient.update(updateRequest, RequestOptions.DEFAULT);
+        System.out.println(updateResponse.getResult());  // UPDATED
+        // 关闭es客户端
+        restClient.close();
+    }
+
+    /**
+     * 删除文档
+     */
+    public static void deleteDoc() throws IOException {
+        // 创建DeleteRequest对象
+        DeleteRequest deleteRequest = new DeleteRequest();
+        // 指定_index和_id
+        deleteRequest.index("user").id("01");
+        // es客户端执行delete操作
+        DeleteResponse deleteResponse = restClient.delete(deleteRequest, RequestOptions.DEFAULT);
+        System.out.println(deleteResponse.getResult());  // DELETED
+        // 关闭es客户端
+        restClient.close();
+    }
+
+    /**
+     * 批量处理
+     */
+    public static void bulkDoc() throws IOException {
+        // 创建BulkRequest对象
+        BulkRequest bulkRequest = new BulkRequest();
+        // 添加批量操作,包括插入、修改、删除
+        bulkRequest.add(new IndexRequest().index("user").id("001").source(XContentType.JSON, "name", "aaa", "age", 19));
+        bulkRequest.add(new UpdateRequest().index("user").id("001").doc(XContentType.JSON, "age", 20));
+        bulkRequest.add(new DeleteRequest().index("user").id("001"));
+        // es客户端执行bulk操作
+        BulkResponse bulkResponse = restClient.bulk(bulkRequest, RequestOptions.DEFAULT);
+        System.out.println(bulkResponse.getTook());  // 7ms
+        // 关闭es客户端
+        restClient.close();
+    }
+
+    /**
+     * 主键查询
+     */
+    public static void getDoc() throws IOException {
+        // 创建GetRequest对象
+        GetRequest getRequest = new GetRequest();
+        // 指定_index和_id
+        getRequest.index("user").id("01");
+        // es客户端执行get操作
+        GetResponse getResponse = restClient.get(getRequest, RequestOptions.DEFAULT);
+        System.out.println(getResponse.getSourceAsString());  // {"age":19,"name":"grubby","sex":"男"}
+        // 关闭es客户端
+        restClient.close();
+    }
+
+    /**
+     * 条件查询
+     */
+    public static void searchDoc() throws IOException {
+        // 创建SearchRequest对象
+        SearchRequest searchRequest = new SearchRequest("war3");
+        // 构造查询条件
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+
+        // 查询所有
+        builder.query(QueryBuilders.matchAllQuery());
+
+        // 单字段查询,es字符串有text(分词)和keyword(不分词)两种类型,match会将查询条件进行分词,多个词条间是or关系
+//        builder.query(QueryBuilders.matchQuery("title", "小米"));
+
+        // 多字段查询
+//        builder.query(QueryBuilders.multiMatchQuery("手机", "title", "category"));
+
+        // 短语查询，相当于%like%
+//        builder.query(QueryBuilders.matchPhraseQuery("title", "米"));
+
+        // 单字段精准查询
+//        builder.query(QueryBuilders.termQuery("name", "grubby"));
+
+        // 多字段精准查询
+//        builder.query(QueryBuilders.termsQuery("name", "grubby", "moon"));
+
+        // 容错查询
+//        builder.query(QueryBuilders.fuzzyQuery("name", "moo").fuzziness(Fuzziness.AUTO));
+
+        // 范围查询
+//        builder.query(QueryBuilders.rangeQuery("age").gte(18).lt(20));
+
+        // 条件查询
+        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
+        boolQuery.must(QueryBuilders.matchQuery("race", "orc"));
+        boolQuery.mustNot(QueryBuilders.matchQuery("name", "fly"));
+        boolQuery.should(QueryBuilders.matchQuery("age", 19));
+//        builder.query(boolQuery);
+
+        // 简单聚合
+        MaxAggregationBuilder aggregationBuilder = AggregationBuilders.max("ageStats").field("age");
+//        builder.aggregation(aggregationBuilder);
+
+        // 分组聚合
+        TermsAggregationBuilder termsAggregationBuilder = AggregationBuilders.terms("ageGroup").field("age");
+//        builder.aggregation(termsAggregationBuilder);
+
+        // 过滤字段
+        String[] includes = {};
+        String[] excludes = {"age"};
+        builder.fetchSource(includes, excludes);
+        // 排序,可以单字段也可以多字段
+//        builder.sort("age", SortOrder.DESC);
+        // 分页,from当前页索引从0开始,size每页显示条数,from = (pageNum - 1) * size
+//        builder.from(0).size(5);
+
+        // 将查询条件添加到请求对象
+        searchRequest.source(builder);
+        // es客户端执行search操作
+        SearchResponse searchResponse = restClient.search(searchRequest, RequestOptions.DEFAULT);
+        System.out.println(searchResponse.getTook());  // 2ms
+        // 遍历结果集
+        SearchHits hits = searchResponse.getHits();
+        System.out.println(hits.getTotalHits());  // 1 hits
+        for (SearchHit hit : hits) {
+            System.out.println(hit.getSourceAsString());  // {"age":18,"name":"grubby","sex":"男"}
         }
+        // 关闭es客户端
+        restClient.close();
     }
 
-    // 根据index名称和docId查询单个document
-    public static String getIndexById(String indexName, String docId) throws IOException {
-        // 获取jest客户端
-        JestClient jestClient = getJestClient();
-        // 封装Get对象,将index名称和文档id作为Builder的参数传入
-        Get get = new Get.Builder(indexName, docId).build();
-        // 执行get查询操作
-        try {
-            DocumentResult result = jestClient.execute(get);
-            return result.getJsonString();
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // 4.关闭连接
-            jestClient.close();
-        }
-        return null;
+    @Data
+    @AllArgsConstructor
+    public static class User {
+        private String name;
+        private String sex;
+        private Integer age;
     }
 
-    // 根据index名称和query条件批量查询document
-    public static List<Map<String, Object>> getIndexByQuery(String indexName) throws IOException {
-        ArrayList<Map<String, Object>> list = new ArrayList<>();
-        // 获取jest客户端
-        JestClient jestClient = getJestClient();
-        // 封装Search对象,将条件查询字符串作为Builder的参数传入
-        Search search= new Search.Builder(generateQueryStr()).addIndex(indexName).build();
-        try {
-            // 执行search查询操作
-            SearchResult result = jestClient.execute(search);
-            // 获取命中结果
-            List<SearchResult.Hit<Map, Void>> hitList = result.getHits(Map.class);
-            // 只取命中结果的"_source"部分
-            for (SearchResult.Hit<Map, Void> hit : hitList) {
-                list.add(hit.source);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            // 关闭连接
-            jestClient.close();
-        }
-        return list;
+    public static void main(String[] args) throws Exception {
+//        doIndex();
+//        insertDoc();
+//        updateDoc();
+//        deleteDoc();
+//        bulkDoc();
+//        getDoc();
+        searchDoc();
     }
-
-    public static Object generateSource() {
-        // 创建样例类对象
-        ArrayList<Map<String, Object>> actorList = new ArrayList<>();
-        HashMap<String, Object> hashMap = new HashMap<>();
-        hashMap.put("id", 1);
-        hashMap.put("name", "grubby");
-        actorList.add(hashMap);
-        return new MovieInfo(1, "war3", 9.5, actorList);
-    }
-
-    public static String generateQueryStr() {
-        // kibana中编写的条件查询字符串
-//    String queryStr =
-//      """
-//        |{
-//        |  "query": {
-//        |    "bool": {
-//        |      "must": [
-//        |        {"match": {"name": "red"}}
-//        |      ],
-//        |      "filter": [
-//        |        {"term": {"actorList.name.keyword": "zhang yi"}}
-//        |      ]
-//        |    }
-//        |  },
-//        |  "from": 0,
-//        |  "size": 20,
-//        |  "sort": [
-//        |    {"doubanScore": {"order": "desc"}}
-//        |  ],
-//        |  "highlight": {
-//        |    "fields": {"name": {}}
-//        |  }
-//        |}
-//        |""".stripMargin
-
-        // 手动拼接不便于维护,应该使用面向对象的方式,通过类来封装查询字符串对象
-        SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
-        // 该字符串主要包含"query" "from" "size" "sort" "highlight"几个部分
-        // 查看源码发现query方法需传入QueryBuilder接口,F4查找实现类,"query"下面包含"bool","must","filter"等,都有相对应的java类或方法
-        sourceBuilder
-                .query(new BoolQueryBuilder()
-                        .must(new MatchQueryBuilder("name", "red"))
-                        .filter(new TermQueryBuilder("actorList.name.keyword", "zhang yi"))
-                )
-                .from(0)
-                .size(10)
-                .sort("doubanScore", SortOrder.DESC)
-                .highlighter(new HighlightBuilder().field("name"));
-        // 将SearchSourceBuilder对象转换成字符串
-        return sourceBuilder.toString();
-    }
-
-    // 使用POJO类封装es的index中的source对象
-    public static class MovieInfo {
-        public Integer id;
-        public String name;
-        public Double doubanScore;
-        public List<Map<String, Object>> actorList;  // [{"name" : "兽王", "id" : 1}]
-
-        public MovieInfo() {
-        }
-
-        public MovieInfo(Integer id, String name, Double doubanScore, List<Map<String, Object>> actorList) {
-            this.id = id;
-            this.name = name;
-            this.doubanScore = doubanScore;
-            this.actorList = actorList;
-        }
-
-        @Override
-        public String toString() {
-            return "MovieInfo{" +
-                    "id=" + id +
-                    ", name='" + name + '\'' +
-                    ", doubanScore=" + doubanScore +
-                    ", actorList=" + actorList +
-                    '}';
-        }
-    }
-
-    public static void main(String[] args) throws IOException {
-        JestClient jestClient = getJestClient();
-        // 测试连接
-        System.out.println(jestClient);
-        // 测试插入数据
-        putIndex("movie_cn");
-        // 测试查询数据
-        System.out.println(getIndexById("movie_cn", "2"));
-        System.out.println(getIndexByQuery("movie_index"));
-    }
-
 }
