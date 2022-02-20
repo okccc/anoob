7b142b75f19e88a65efa66d4db1a440f1846398c
okccc <1573976179@qq.com>
2022-02-20 11:25:24 +0800

    ...

--- src/main/java/com/okccc/realtime/app/dws/ProductStatsApp.java
+++ src/main/java/com/okccc/realtime/app/dws/ProductStatsApp.java
@@ -60,33 +60,32 @@ public class ProductStatsApp {
          *
          * -- clickhouse建表语句
          * create table if not exists product_stats (
-         *   stt               datetime,
-         *   edt               datetime,
-         *   sku_id            bigint,
+         *   stt               DateTime,
+         *   edt               DateTime,
+         *   sku_id            UInt64,
          *   sku_name          String,
-         *   sku_price         double,
-         *   spu_id            bigint,
+         *   sku_price         Decimal64(2),
+         *   spu_id            UInt64,
          *   spu_name          String,
-         *   tm_id             bigint,
+         *   tm_id             UInt64,
          *   tm_name           String,
-         *   category3_id      bigint,
+         *   category3_id      UInt64,
          *   category3_name    String,
-         *   click_ct          int,
-         *   display_ct        int,
-         *   favor_ct          int,
-         *   cart_ct           int,
-         *   order_ct          int,
-         *   order_sku_num     int,
-         *   order_amount      double,
-         *   paid_order_ct     int,
-         *   payment_amount    double,
-         *   refund_order_ct   int,
-         *   refund_amount     double,
-         *   comment_ct        int,
-         *   good_comment_ct   int,
-         *   ts                bigint
-         *  ) engine = MergeTree partition by toYYYYMMDD(stt)
-         *    order by (stt,edt,sku_id,sku_name,sku_price,spu_id,spu_name,tm_id,tm_name,category3_id,category3_name);
+         *   click_ct          UInt64,
+         *   display_ct        UInt64,
+         *   favor_ct          UInt64,
+         *   cart_ct           UInt64,
+         *   order_ct          UInt64,
+         *   order_sku_num     UInt64,
+         *   order_amount      Decimal64(2),
+         *   paid_order_ct     UInt64,
+         *   payment_amount    Decimal64(2),
+         *   refund_order_ct   UInt64,
+         *   refund_amount     Decimal64(2),
+         *   comment_ct        UInt64,
+         *   good_comment_ct   UInt64,
+         *   ts                UInt64
+         *  ) engine = ReplacingMergeTree(ts) partition by toYYYYMMDD(stt) order by (stt,edt,sku_id);
          */
 
         // 1.创建流处理执行环境
