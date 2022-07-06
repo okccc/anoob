511af62c9d767aa5acf513688615f77a282ad6cd
okccc <1573976179@qq.com>
2022-07-06 16:59:09 +0800

    代码报错精准定位问题根源

--- src/main/java/com/okccc/realtime/utils/PropertiesUtil.java
+++ src/main/java/com/okccc/realtime/utils/PropertiesUtil.java
@@ -13,9 +13,9 @@ public class PropertiesUtil {
     public static Properties load(String fileName) {
         // 创建Properties对象
         Properties prop = new Properties();
-        // 加载resources资源文件
         try {
-            // ClassLoader有个小陷阱,代码打包提交到linux后会读不到resources资源文件导致空指针异常
+            // ClassLoader陷阱：代码打包提交到linux后会读不到resources资源文件导致空指针异常
+            // 代码报错不要慌,Caused by: XxxException下面找到自己写的那几行代码,定位问题根源
 //            prop.load(ClassLoader.getSystemClassLoader().getResourceAsStream(fileName));
             prop.load(Thread.currentThread().getContextClassLoader().getResourceAsStream(fileName));
         } catch (IOException e) {
