6d05ec4995f246db8f45fa947c7cc4305fb328f7
okccc <1573976179@qq.com>
2022-08-18 15:47:43 +0800

    shellcheck-SC2006: Use $(...) notation instead of legacy backticked `...`

---
+++ src/main/java/com/okccc/warehouse/shell/yarn任务监控.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 待监控的任务
+tables='ghs_user node_flow_record lesson_flow_record student_record ghs_wechat_account lesson'
+
+for table in ${tables}
+do
+    # Use $(...) notation instead of legacy backticked `...`. See SC2006
+    res=`yarn application -list | awk '{print $2}' | grep -x ${table}`
+done
