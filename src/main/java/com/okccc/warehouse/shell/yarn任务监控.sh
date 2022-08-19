6c50d8b305de2df9b3eaad6339042bea1f173cf0
okccc <1573976179@qq.com>
2022-08-19 10:03:22 +0800

    shellcheck-SC2086: Double quote to prevent globbing and word splitting

--- src/main/java/com/okccc/warehouse/shell/yarn任务监控.sh
+++ src/main/java/com/okccc/warehouse/shell/yarn任务监控.sh
@@ -6,5 +6,6 @@ tables='ghs_user node_flow_record lesson_flow_record student_record ghs_wechat_a
 for table in ${tables}
 do
     # Use $(...) notation instead of legacy backticked `...`. See SC2006
-    res=`yarn application -list | awk '{print $2}' | grep -x ${table}`
+    # Double quote to prevent globbing and word splitting. See SC2086
+    res=$(yarn application -list | awk '{print $2}' | grep -x ${table})
 done
