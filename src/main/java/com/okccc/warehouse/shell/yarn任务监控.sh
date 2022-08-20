9e9b9abdeef5ef7f05c5017cfe464e30bf75e65d
okccc <1573976179@qq.com>
2022-08-20 09:14:08 +0800

    shell脚本求字符串长度

--- src/main/java/com/okccc/warehouse/shell/yarn任务监控.sh
+++ src/main/java/com/okccc/warehouse/shell/yarn任务监控.sh
@@ -6,6 +6,12 @@ tables='ghs_user node_flow_record lesson_flow_record student_record ghs_wechat_a
 for table in ${tables}
 do
     # Use $(...) notation instead of legacy backticked `...`. See SC2006
-    # Double quote to prevent globbing and word splitting. See SC2086
+    # Double quote to prevent globbing and word splitting. See SC2086.
+#    res=`yarn application -list | awk '{print $2}' | grep -x ${table}`
     res=$(yarn application -list | awk '{print $2}' | grep -x ${table})
+    echo "${res}"
+    # 求字符串长度str='hello' 1.${#str} 2.expr length ${str} 3.echo ${str}|wc -L 4.echo ${str}|awk '{print length($0)}'
+    if [ ${#res} -eq 0 ]; then
+        python3 /data/projects-app/flink2hdfs/alarm.py "${table}"
+    fi
 done
