0ad197d10fe38724b2c1446304f47f9a09c2a44b
okccc <1573976179@qq.com>
2022-09-20 10:21:15 +0800

    shellcheck-SC2219: Instead of let expr, prefer (( expr ))

--- src/main/java/com/okccc/warehouse/shell/while循环补数据.sh
+++ src/main/java/com/okccc/warehouse/shell/while循环补数据.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-table=$1
-partitions=$2
+# 判断参数个数
+if [ $# -eq 2 ]; then
+    table=$1
+    days=$2
+else
+    echo "wrong arg[] numbers"
+    exit 1
+fi
 
 num=1
 
-while [${num} -lt ${partitions}]
+while ${num} -lt "${days}"
 do
-    echo ${num}
-    stat_date=`date -d "-${num} day" +%Y%m%d`  # (注意：是" ",不是' ')
-    echo "当前分区是: ${stat_date}"
-
-    # 要执行的代码部分
-    hive -hivevar data_date=${stat_date} -f /data/hive/hqls/${table}.sql
-
-    let num+=1
-
+    stat_date=$(date -d "-${num} day" +%Y%m%d)
+    echo "${stat_date}"
+    hive -hivevar data_date="${stat_date}" -f /data/hive/hqls/"${table}".sql
+    # SC2219: Instead of let expr, prefer (( expr )) .
+#    let num+=1
+    ((num+=1))
 done
