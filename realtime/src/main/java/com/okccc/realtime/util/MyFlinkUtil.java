e3f2de2ae43158c131b846727e57dee35a22d993
okccc <1573976179@qq.com>
2022-12-13 10:19:10 +0800

    kafka broker事务最大超时时间

--- realtime/src/main/java/com/okccc/realtime/util/MyFlinkUtil.java
+++ realtime/src/main/java/com/okccc/realtime/util/MyFlinkUtil.java
@@ -18,15 +18,28 @@ import java.nio.charset.StandardCharsets;
 import java.time.ZoneId;
 import java.util.List;
 import java.util.Properties;
+import java.util.UUID;
 
 /**
- * Author: okccc
- * Date: 2021/10/4 下午7:41
- * Desc: flink读写kafka的工具类
+ * @Author: okccc
+ * @Date: 2021/10/4 19:41
+ * @Desc: flink读写kafka的工具类
  * https://nightlies.apache.org/flink/flink-docs-release-1.13/zh/docs/connectors/datastream/kafka/
  * https://nightlies.apache.org/flink/flink-docs-release-1.13/zh/docs/connectors/datastream/file_sink/
+ *
+ * 常见错误
+ * Caused by: org.apache.kafka.common.errors.ProducerFencedException: Producer attempted an operation with an old epoch.
+ * Either there is a newer producer with the same transactionalId, or the producer's transaction has been expired by the broker.
+ * kafka生产者exactly_once：幂等性只能保证单分区单会话内数据不重复,完全不重复还得在幂等性的基础上开启事务
+ *
+ * Caused by: org.apache.kafka.common.KafkaException: Unexpected error in InitProducerIdResponse;
+ * The transaction timeout is larger than the maximum value allowed by the broker (as configured by transaction.max.timeout.ms).
+ * https://nightlies.apache.org/flink/flink-docs-release-1.13/zh/docs/connectors/datastream/kafka/#kafka-producer-%E5%92%8C%E5%AE%B9%E9%94%99
+ * Kafka broker事务最大超时时间transaction.max.timeout.ms=15分钟,而FlinkKafkaProducer的transaction.timeout.ms=1小时,
+ * 因此在使用Semantic.EXACTLY_ONCE模式之前应该调小transaction.timeout.ms的值
  */
 public class MyFlinkUtil {
+
     private static final String KAFKA_SERVER = "localhost:9092";
     private static final String DEFAULT_TOPIC = "default";
 
@@ -35,20 +48,20 @@ public class MyFlinkUtil {
      */
     public static FlinkKafkaConsumer<String> getKafkaSource(String topic, String groupId) {
         // 消费者属性配置
-        Properties props = new Properties();
-        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
-        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        Properties prop = new Properties();
+        prop.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+        prop.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         // 创建flink消费者对象
-        return new FlinkKafkaConsumer<>(topic, new SimpleStringSchema(), props);
+        return new FlinkKafkaConsumer<>(topic, new SimpleStringSchema(), prop);
     }
 
     public static FlinkKafkaConsumer<String> getKafkaSource(List<String> topics, String groupId) {
         // 消费者属性配置
-        Properties props = new Properties();
-        props.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
-        props.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        Properties prop = new Properties();
+        prop.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+        prop.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         // 创建flink消费者对象
-        return new FlinkKafkaConsumer<>(topics, new SimpleStringSchema(), props);
+        return new FlinkKafkaConsumer<>(topics, new SimpleStringSchema(), prop);
     }
 
     /**
@@ -57,8 +70,9 @@ public class MyFlinkUtil {
     public static FlinkKafkaProducer<String> getKafkaSink(String topic) {
         // 生产者属性配置
         Properties prop = new Properties();
-        prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
-        prop.setProperty(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, 900 * 1000 + "");
+        prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+        prop.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, UUID.randomUUID().toString());
+        prop.put(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, 15 * 60 * 1000);
         // 这个只能保证AT_LEAST_ONCE(96行源码)
 //        return new FlinkKafkaProducer<>(KAFKA_SERVER, topic, new SimpleStringSchema());
         // 创建flink生产者对象
@@ -81,8 +95,9 @@ public class MyFlinkUtil {
     public static <T> FlinkKafkaProducer<T> getKafkaSinkBySchema(KafkaSerializationSchema<T> kafkaSerializationSchema) {
         // 生产者属性配置
         Properties prop = new Properties();
-        prop.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
-        prop.setProperty(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, 900 * 1000 + "");
+        prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+        prop.put(ProducerConfig.TRANSACTIONAL_ID_CONFIG, UUID.randomUUID().toString());
+        prop.put(ProducerConfig.TRANSACTION_TIMEOUT_CONFIG, 15 * 60 * 1000);
         // 创建flink生产者对象
         return new FlinkKafkaProducer<>(DEFAULT_TOPIC, kafkaSerializationSchema, prop, FlinkKafkaProducer.Semantic.EXACTLY_ONCE);
     }
@@ -104,6 +119,7 @@ public class MyFlinkUtil {
      */
     public static FileSink<String> getHdfsSink(String output) {
         return FileSink
+                // 行编码格式 Row-encoded Formats
                 .forRowFormat(new Path(output), new SimpleStringEncoder<String>("UTF-8"))
                 // 桶分配
                 .withBucketAssigner(new HiveBucketAssigner<>("yyyyMMdd", ZoneId.of("Asia/Shanghai")))
@@ -116,6 +132,7 @@ public class MyFlinkUtil {
                                 .withMaxPartSize(1024 * 1024 * 128)
                                 .build()
                 ).build();
+        // 批量编码格式 Bulk-encoded Formats: Parquet Format、Avro Format、ORC Format
     }
 
     public static class HiveBucketAssigner<IN> extends DateTimeBucketAssigner<IN> {
