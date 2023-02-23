849cbd6ac369653f6d3841ad2fe2c74406e14410
okccc <1573976179@qq.com>
2023-02-23 10:08:14 +0800

    flink任务配置检查点和状态后端

--- flink/src/main/java/com/okccc/flink/FlinkUtil.java
+++ flink/src/main/java/com/okccc/flink/FlinkUtil.java
@@ -1,15 +1,21 @@
 package com.okccc.flink;
 
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.connector.base.DeliveryGuarantee;
 import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.contrib.streaming.state.EmbeddedRocksDBStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.kafka.clients.producer.ProducerConfig;
 
 import java.util.Properties;
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @Author: okccc
@@ -37,6 +43,37 @@ public class FlinkUtil {
 
     // kafka地址
     private static final String KAFKA_SERVER = "localhost:9092";
+
+    /**
+     * 配置检查点和状态后端
+     */
+    public static StreamExecutionEnvironment getExecutionEnvironment() {
+        // 创建流处理环境
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        // 禁用算子链,方便定位导致反压的具体算子
+        env.disableOperatorChaining();
+        // 设置状态后端
+//        env.setStateBackend(new HashMapStateBackend());
+        env.setStateBackend(new EmbeddedRocksDBStateBackend());
+        // 检查点时间间隔：通常1~5分钟,查看Checkpoints - Summary - End to End Duration,综合考虑性能和时效性
+        env.enableCheckpointing(TimeUnit.MINUTES.toMillis(2), CheckpointingMode.EXACTLY_ONCE);
+        CheckpointConfig config = env.getCheckpointConfig();
+        // 检查点存储路径
+        config.setCheckpointStorage("hdfs://cdh1/flink/ck");
+        // 检查点超时时间
+        config.setCheckpointTimeout(TimeUnit.MINUTES.toMillis(5));
+        // 检查点可容忍的连续失败次数
+        config.setTolerableCheckpointFailureNumber(3);
+        // 检查点最小等待间隔,通常是时间间隔一半
+        config.setMinPauseBetweenCheckpoints(TimeUnit.MINUTES.toMillis(1));
+        // 检查点保留策略：job取消时默认会自动删除检查点,可以保留防止任务故障重启失败,还能从检查点恢复任务,后面手动删除即可
+        config.setExternalizedCheckpointCleanup(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+        // 重启策略：重试间隔调大一点,不然flink监控页面一下子就刷新过去变成job failed,看不到具体异常信息
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, TimeUnit.MINUTES.toMillis(1)));
+        // 本地调试时要指定能访问hadoop的用户
+        System.setProperty("HADOOP_USER_NAME", "deploy");
+        return env;
+    }
 
     /**
      * 从kafka读数据的消费者
