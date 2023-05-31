30b7cdda0ff8cd84b81a69624e38455e5c843382
okccc <1573976179@qq.com>
2023-05-31 17:51:31 +0800

    ...

--- anoob-hadoop/src/main/java/com/okccc/bean/UserInfo.java
+++ anoob-hadoop/src/main/java/com/okccc/bean/UserInfo.java
@@ -5,18 +5,18 @@ import lombok.*;
 /**
  * @Author: okccc
  * @Date: 2022/12/12 15:01
- * @Desc: 用户类
+ * @Desc: 用户信息类
  */
 @Data  // lombok可以简化JavaBean开发,会自动实现属性的get&set方法,可通过Structure查看
 @NoArgsConstructor  // 无参构造器
 @AllArgsConstructor  // 全参构造器,如果不需要全部参数可以手动指定参数实现
 @EqualsAndHashCode  // equals和hashCode方法
 @ToString  // toString方法
-public class User {
+public class UserInfo {
 
-    private int id;
+    private int userId;
 
-    private String name;
+    private String userName;
 
     private int age;
 }
