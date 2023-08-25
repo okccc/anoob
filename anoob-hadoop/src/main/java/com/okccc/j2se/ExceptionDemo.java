7541af37cb20a04d4a761d5abe3c2c1f0123e06d
okccc <1573976179@qq.com>
2023-08-25 10:29:56 +0800

    java异常捕获和抛出

--- anoob-hadoop/src/main/java/com/okccc/j2se/ExceptionDemo.java
+++ anoob-hadoop/src/main/java/com/okccc/j2se/ExceptionDemo.java
@@ -15,11 +15,12 @@ import java.net.URLDecoder;
  * java.lang.Throwable
  *     |---- java.lang.Error: java虚拟机无法解决的严重问题,比如jvm系统内部错误、资源耗尽等严重情况,一般不编写针对性的代码进行处理
  * 	   |---- java.lang.Exception: 可以处理的异常
- *         |---- 编译时异常(checked)必须处理,不然编译不通过
+ *         |---- 编译时异常(checked)必须处理不然编译不通过,尽量捕获不要抛给上层
+ *             |---- SQLException
  * 		       |---- IOException
  * 			       |---- FileNotFoundException
  * 			   |---- ClassNotFoundException
- *         |---- 运行时异常(unchecked, RuntimeException)
+ *         |---- 运行时异常(unchecked, RuntimeException),可以抛给调用者尽可能晚的处理异常
  *             |---- NullPointerException
  *             |---- ArrayIndexOutOfBoundsException
  *             |---- ClassCastException
@@ -27,10 +28,7 @@ import java.net.URLDecoder;
  *             |---- InputMismatchException
  *             |---- ArithmeticException
  *
- * 异常处理机制
- * "抓"：通过try-catch或者throws处理异常,继续执行后面代码
- * "抛"：程序出现异常时会生成对应的异常类对象并抛出,后面代码不会执行,异常对象可以是系统自动生成也可以是手动生成并throw
- * 1.捕获异常
+ * 捕获异常,代码继续执行
  * try {
  *     // 可能出现异常的代码,子类重写父类方法时,父类有异常子类必须抛出相同异常或其子类,父类没有异常子类也不能抛异常只能try-catch
  * } catch (异常类型1 变量名1) {
@@ -41,14 +39,12 @@ import java.net.URLDecoder;
  *     // 无论是否异常最终都会执行的代码,比如数据库连接、输入输出流、网络编程socket这些资源jvm无法回收,需要手动释放
  * }
  *
- * 2.抛出异常
- * throws将程序可能出现的异常类型写在方法声明处,执行方法出现该异常时就会被抛出,后面代码不会执行
- * try-catch处理异常,throws抛出异常,调用者可以继续往上抛给main方法,再不处理就会抛给jvm在控制台打印错误堆栈日志
+ * 抛出异常,程序终止
+ * throws是在方法声明处,后面跟异常类型,将异常抛给调用者处理,调用者可以继续往上抛给main方法,再不处理就会抛给jvm在控制台打印错误堆栈日志
  * 比如开发MVC框架时,Mapper -> Service -> Controller逐层往上抛,视图直接面向用户不能再抛了必须try catch处理
+ * throw是在方法体中,后面跟异常对象,两者作用是一样的,都是为了终止程序
  *
- * throw和throws区别
- * throw是手动生成异常,后面跟异常对象,定义在方法体中(不常用)
- * throws是异常处理的第二种方式,后面跟异常类型,定义在方法声明中
+ * 总结：异常捕获或抛出取决于当前程序的设计需求,如果想跳出当前异常继续往下执行就捕获,如果想在更高层对异常集中处理就抛出
  */
 public class ExceptionDemo {
 
@@ -59,22 +55,24 @@ public class ExceptionDemo {
 //        main(args);  // 栈溢出：java.lang.StackOverflowError
 //        Integer[] arr = new Integer[1024 * 1024 * 1024];  // 堆溢出：java.lang.OutOfMemoryError: Requested array size exceeds VM limit
 
-        Integer a = 10;
-        Integer b = 0;
+        int a = 10;
+        int b = 0;
         try {
             int i = a / b;
         } catch (ArithmeticException e) {
-            e.printStackTrace();  // 在控制台打印错误堆栈信息,本地调试用
-            logger.error("error", e);  // 线上代码还是要记录下日志信息,方便排错
+//            e.printStackTrace();  // 在控制台打印错误堆栈信息,本地调试用
+//            logger.error("error", e);  // 线上代码还是要记录下日志信息,方便排错
+            throw new ArithmeticException(e.getMessage());
         } finally {
+            // finally代码块无论如何都会执行
             System.out.println("aaa");
         }
-        // try-catch捕获异常后下面代码继续执行
+
+        // try-catch捕获异常后下面代码继续执行,但如果throw手动抛出异常则程序终止
         System.out.println("bbb");
 
-        String str = "%22os_name%22%3A%22android%22";
-        System.out.println(URLDecoder.decode(str, "sb"));  // java.io.UnsupportedEncodingException: sb
         // throws抛出异常后下面代码不会执行
+        URLDecoder.decode("%22os_name%22%3A%22android%22", "sb");
         System.out.println("ccc");
     }
 }
