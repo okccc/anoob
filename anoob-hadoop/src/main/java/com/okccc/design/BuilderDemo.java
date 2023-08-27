4e9f5d5bd2eab07aadede02e11f769aed9923706
okccc <1573976179@qq.com>
2023-08-27 09:50:13 +0800

    java构造者模式

--- anoob-hadoop/src/main/java/com/okccc/design/BuilderDemo.java
+++ anoob-hadoop/src/main/java/com/okccc/design/BuilderDemo.java
@@ -3,95 +3,98 @@ package com.okccc.design;
 /**
  * @Author: okccc
  * @Date: 2020/12/31 2:09 下午
- * @Desc: java构造者模式
+ * @Desc: java构造者模式：解决多参数构造方法的初始化问题
+ *
+ * 1.在类的内部创建静态内部类Builder
+ * 2.Builder类的属性和外部类保持一致,由Builder类实现属性的setXxx()方法,并最终提供build()方法返回外部类对象
+ * 优点：通过Builder类一步一步构建复杂对象,可以输入任意组合的参数,避免多参数构造方法重载出错,还不用写大量构造器
+ *
+ * es/spark/flink都大量使用了构造者模式
+ * io.searchbox.client.config.HttpClientConfig
+ * io.searchbox.core.Index/Get/Update/Delete/Search
+ * org.apache.spark.sql.SparkSession
+ * org.apache.flink.connector.kafka.source.KafkaSource/sink.KafkaSink
+ * org.apache.flink.connector.file.src.FileSource/sink.FileSink
+ * org.apache.flink.connector.jdbc.JdbcExecutionOptions/JdbcConnectionOptions
+ * org.apache.flink.connector.elasticsearch.sink.Elasticsearch7SinkBuilder
+ * org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig
+ * org.apache.flink.api.common.state.StateTtlConfig
  */
 public class BuilderDemo {
-    public static void main(String[] args) {
-        /*
-         * 构造者模式：解决多参数构造方法的初始化问题
-         * 1.在类的内部创建静态或非静态内部类Builder
-         * 2.Builder类的属性和外部类保持一致,由Builder类实现属性的setXxx方法,并最终提供build方法返回外部类对象
-         * 优点：通过Builder类一步一步构建复杂对象,可以任意组合输入参数,避免多参数构造方法重载出错,还不用写过多的构造器
-         *
-         * es/spark/flink都大量使用了构造者模式
-         * io.searchbox.client.config.HttpClientConfig
-         * io.searchbox.core.Index/Get/Update/Delete/Search
-         * org.apache.spark.sql.SparkSession
-         * org.apache.flink.connector.jdbc.JdbcExecutionOptions/JdbcConnectionOptions
-         * org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig
-         * org.apache.flink.streaming.connectors.elasticsearch6.ElasticsearchSink
-         * org.apache.flink.streaming.api.functions.sink.filesystem.StreamingFileSink
-         * org.apache.flink.api.common.state.StateTtlConfig
-         */
-        User user = new User.Builder().setName("fly").setMobile("111").setEmail("orc@qq.com").build();
-        System.out.println(user.getName());
+
+    private final String name;
+    private final String mobile;
+    private final String email;
+
+    public BuilderDemo(String name, String mobile, String email) {
+        this.name = name;
+        this.mobile = mobile;
+        this.email = email;
     }
 
-    // 普通模式
-    public static class User01 {
-        private String name;
-        private String address;
-        private String email;
-        // 包含name和address的构造方法
-        public User01(String name, String address) {
-            this.name = name;
-            this.address = address;
-        }
-        // 包含name和email的构造方法
-        // 当构造方法的参数个数和参数类型完全一样时,方法重载会报错：'User01(String, String)' is already defined
-//        public User01(String name, String email) {
-//            this.name = name;
-//            this.address = address;
-//        }
+    public static Builder builder() {
+        return new Builder();
     }
 
-    // 构造者模式
-    public static class User {
+    public String getName() {
+        return name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public static class Builder {
         private String name;
         private String mobile;
         private String email;
 
-        public User(Builder builder) {
-            this.name = builder.name;
-            this.mobile = builder.mobile;
-            this.email = builder.email;
+        public Builder setName(String name) {
+            this.name = name;
+            return this;
         }
 
-        public String getName() {
-            return name;
+        public Builder setMobile(String mobile) {
+            this.mobile = mobile;
+            return this;
         }
 
-        public String getMobile() {
-            return mobile;
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
         }
 
-        public String getEmail() {
-            return email;
-        }
-
-        public static class Builder {
-            private String name;
-            private String mobile;
-            private String email;
-
-            public Builder setName(String name) {
-                this.name = name;
-                return this;
-            }
-
-            public Builder setMobile(String mobile) {
-                this.mobile = mobile;
-                return this;
-            }
-
-            public Builder setEmail(String email) {
-                this.email = email;
-                return this;
-            }
-
-            public User build() {
-                return new User(this);
-            }
+        public BuilderDemo build() {
+            return new BuilderDemo(name, mobile, email);
         }
     }
+
+    public static void main(String[] args) {
+        BuilderDemo demo01 = BuilderDemo.builder().setName("grubby").setMobile("111").setEmail("orc@qq.com").build();
+        BuilderDemo demo02 = new BuilderDemo.Builder().setName("moon").setMobile("222").setEmail("ne@qq.com").build();
+        System.out.println(demo01.getName() + " - " + demo01.getMobile() + " - " + demo01.getEmail());
+        System.out.println(demo02.getName() + " - " + demo02.getMobile() + " - " + demo02.getEmail());
+    }
 }
+
+// 普通模式
+//public class User01 {
+//    private String name;
+//    private String mobile;
+//    private String email;
+//
+//    public User01(String name, String mobile) {
+//        this.name = name;
+//        this.mobile = mobile;
+//    }
+//
+//    // 当构造方法的参数个数和参数类型完全一样时,方法重载会报错：'User01(String, String)' is already defined
+//    public User01(String name, String email) {
+//        this.name = name;
+//        this.email = email;
+//    }
+//}
