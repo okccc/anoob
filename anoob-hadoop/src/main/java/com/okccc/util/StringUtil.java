eb98b0ebc611f58c229e815298aa5d7e041887b6
okccc <1573976179@qq.com>
2023-09-29 09:46:48 +0800

    ...

--- anoob-hadoop/src/main/java/com/okccc/util/StringUtil.java
+++ anoob-hadoop/src/main/java/com/okccc/util/StringUtil.java
@@ -4,7 +4,11 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONException;
 import com.alibaba.fastjson.JSONObject;
 
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
 import java.net.URLDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
 import java.util.HashMap;
 
 /**
@@ -27,7 +31,9 @@ public class StringUtil {
         }
     }
 
-    // 将main方法传入的字符串参数解析成键值对,类似hive函数str_to_map
+    /**
+     * 将main方法传入的字符串参数解析成键值对,类似hive函数str_to_map
+     */
     public static HashMap<String, String> strToMap(String args) {
         // orc=grubby&ne=moon&hum=sky&ud=ted
         HashMap<String, String> hashMap = new HashMap<>();
@@ -39,7 +45,9 @@ public class StringUtil {
         return hashMap;
     }
 
-    // 截断字符串两侧逗号
+    /**
+     * 截断字符串两侧逗号
+     */
     public static String trimComma(String str) {
         String result = "";
         String tmp = "";
@@ -52,8 +60,10 @@ public class StringUtil {
         return result;
     }
 
-    // \r(return)是回车符,光标移到本行开头,后面字符会覆盖前面字符
-    // \n(newLine)是换行符,光标移到下一行,Linux系统\n换行,Windows系统\r\n换行
+    /**
+     * \r(return)是回车符,光标移到本行开头,后面字符会覆盖前面字符
+     * \n(newLine)是换行符,光标移到下一行,Linux系统\n换行,Windows系统\r\n换行
+     */
     public static String removeEscape(String str) {
         System.out.println("hello\rworld");
         System.out.println("hello\nworld");
@@ -123,7 +133,9 @@ public class StringUtil {
         return str;
     }
 
-    // 解析canal抓取binlog返回的数据
+    /**
+     * 解析canal抓取binlog返回的数据
+     */
     public static String getCanalData(String columns, JSONObject data, JSONObject jsonObject) {
         StringBuilder sb = new StringBuilder();
         String[] arr = columns.split(";");
@@ -154,16 +166,24 @@ public class StringUtil {
                 "审批：20230519(002号\t露露)"));
         System.out.println(decode("%22name%22%3D%22grubby%22%26%22age%22%3D%2218%22"));  // "name"="grubby"&"age"="18"
 
-        String str = "{\"@timestamp\":\"31/Jul/2023:10:37:11 +0800\",\"hostname\":\"tx-prod-bigdata-01\",\"ip\":\"120.32.103.75\",\"Method\":\"POST\",\"referer\":\"-\",\"request\":\"POST /home HTTP/1.1\",\"request_body\":\"v=3&e=%7B%22session_id%22%3A1690770146461%2C%22language%22%3A%22Chinese%22%2C%22user_id%22%3A%22c57921f5bd5e4b218d73e42c83e9945e%22%2C%22country%22%3A%22China%20mainland%22%2C%22device_id%22%3A%2220471BE4-0434-4641-9304-4246F41BF96B%22%2C%22version_name%22%3A%2211.0.1%22%2C%22platform%22%3A%22iOS%22%2C%22carrier%22%3A%22%E4%B8%AD%E5%9B%BD%E7%A7%BB%E5%8A%A8%22%2C%22timestamp%22%3A1690771006362%7D&upload_time=1690771030913\",\"status\":\"200\",\"agent\":\"niuwa/11.0.1.2101131825 CFNetwork/1408.0.4 Darwin/22.5.0\"}";
-        System.out.println(isJsonFormat(str));  // true
-        System.out.println(isJsonFormat(decode(str)));  // false,对整个字符串解码可能会导致数据格式变成非json
+        String log = "{\"@timestamp\":\"31/Jul/2023:10:37:11 +0800\",\"hostname\":\"tx-prod-bigdata-01\",\"ip\":\"120.32.103.75\",\"Method\":\"POST\",\"referer\":\"-\",\"request\":\"POST /home HTTP/1.1\",\"request_body\":\"v=3&e=%7B%22session_id%22%3A1690770146461%2C%22language%22%3A%22Chinese%22%2C%22user_id%22%3A%22c57921f5bd5e4b218d73e42c83e9945e%22%2C%22country%22%3A%22China%20mainland%22%2C%22device_id%22%3A%2220471BE4-0434-4641-9304-4246F41BF96B%22%2C%22version_name%22%3A%2211.0.1%22%2C%22platform%22%3A%22iOS%22%2C%22carrier%22%3A%22%E4%B8%AD%E5%9B%BD%E7%A7%BB%E5%8A%A8%22%2C%22timestamp%22%3A1690771006362%7D&upload_time=1690771030913\",\"status\":\"200\",\"agent\":\"niuwa/11.0.1.2101131825 CFNetwork/1408.0.4 Darwin/22.5.0\"}";
+        System.out.println(isJsonFormat(log));  // true
+        System.out.println(isJsonFormat(decode(log)));  // false,对整个字符串解码可能会导致数据格式变成非json
         // 只对字符串内部的编码部分进行解码
-        JSONObject jsonObject = JSON.parseObject(str);
+        JSONObject jsonObject = JSON.parseObject(log);
         String newBody = decode(jsonObject.getString("request_body"));
         jsonObject.put("request_body", newBody);
         // 获取内部字段
         HashMap<String, String> hashMap = strToMap(newBody);
         String version = JSON.parseObject(hashMap.get("e")).getString("version_name");
         System.out.println(version);  // 11.0.1
+
+        String str = "13888888888";
+        String key = "mga93UeZ2s#&4Oc3";
+        String encryptedStr = encrypt(str, key);
+        System.out.println("加密后：" + encryptedStr);
+        String decryptedStr = decrypt(encryptedStr, key);
+        System.out.println("解密后：" + decryptedStr);
+        System.out.println(str.equals(decryptedStr) ? "success" : "fail");
     }
 }
