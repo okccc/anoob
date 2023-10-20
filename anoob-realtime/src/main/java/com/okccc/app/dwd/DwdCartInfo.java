e2c00b4c310e3c1aa04998594149ab2eaefee4cf
okccc <1573976179@qq.com>
2023-10-20 09:54:10 +0800

    lookup cache

--- anoob-realtime/src/main/java/com/okccc/app/dwd/DwdCartInfo.java
+++ anoob-realtime/src/main/java/com/okccc/app/dwd/DwdCartInfo.java
@@ -20,4 +20,11 @@ package com.okccc.app.dwd;
  * lookup join主要用于flink sql表和外部系统(mysql)进行维度关联,要求主表必须有处理时间字段,维表由lookup连接器生成
  * 维度数据是有时效性的,所以需要时间字段来对数据版本进行标识,flink sql通常使用PROCTIME()函数获取当前系统时间作为处理时间字段
  * 事实表和维度表关联用lookup join,类似DataStream中的map算子通过jdbc查询mysql获取补充字段,事实表之间关联用interval join
+ *
+ * lookup cache
+ * The lookup cache is used to improve performance of temporal join the JDBC connector.
+ * The oldest rows in cache will be expired when the cache hit to the max cached rows lookup.cache.max-rows or when the row exceeds the max time to live lookup.cache.ttl.
+ * users can tune lookup.cache.ttl to a smaller value to have a better fresh data, but this may increase the number of requests send to database. So this is a balance between throughput and correctness.
+ * 超过最大缓存行数会清除最老的记录,存活时间超过ttl也会清除
+ * 缓存可以提高查询速度,但缓存可能不是最新的,将ttl调小尽可能获取最新数据但会增加访问数据库的次数,所以这是吞吐量和准确性之间的平衡
  */
