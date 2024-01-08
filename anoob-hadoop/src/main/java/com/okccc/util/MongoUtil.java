d1e7be2ae2e000f9ee27e13070112b24bac2ab1f
okccc <1573976179@qq.com>
2024-01-08 09:54:41 +0800

    mongodb条件查询

--- anoob-hadoop/src/main/java/com/okccc/util/MongoUtil.java
+++ anoob-hadoop/src/main/java/com/okccc/util/MongoUtil.java
@@ -2,10 +2,7 @@ package com.okccc.util;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
-import com.mongodb.client.FindIterable;
-import com.mongodb.client.MongoCollection;
-import com.mongodb.client.MongoCursor;
-import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.*;
 import com.mongodb.client.model.Filters;
 import org.bson.Document;
 
@@ -34,29 +31,35 @@ public class MongoUtil {
      * 通用查询
      */
     public static void common() {
-        // 查询所有库
+        // 遍历所有库
         for (String databaseName : mongoClient.listDatabaseNames()) {
             System.out.println(databaseName);
         }
 
-        // 查询库的所有集合
+        // 遍历库的所有集合
         MongoDatabase db = mongoClient.getDatabase("WAR3");
         for (String collectionName : db.listCollectionNames()) {
             System.out.println(collectionName);
         }
 
-        // 查询集合的所有文档
+        // 遍历集合的所有文档
         MongoCollection<Document> collection = db.getCollection("users");
         System.out.println(collection.countDocuments());
         for (Document document : collection.find()) {
             System.out.println(document);
+        }
+
+        // 遍历集合的索引
+        ListIndexesIterable<Document> listIndexes = collection.listIndexes();
+        for (Document listIndex : listIndexes) {
+            System.out.println("listIndex = " + listIndex);
         }
     }
 
     /**
      * 根据_id查询
      */
-    public static String getDocumentById(String dbName, String collectionName, String id, String key) {
+    public static String getValue(String dbName, String collectionName, String id, String key) {
         // 选择库
         MongoDatabase db = mongoClient.getDatabase(dbName);
         // 选择集合
@@ -73,7 +76,67 @@ public class MongoUtil {
         return res;
     }
 
+    /**
+     * 根据非_id单条件查询
+     */
+    public static String getValue(String dbName, String collectionName, String field1, String value1, String field2) {
+        // 选择库
+        MongoDatabase db = mongoClient.getDatabase(dbName);
+        // 选择集合
+        MongoCollection<Document> collection = db.getCollection(collectionName);
+        // 条件查询
+        FindIterable<Document> documents = collection.find(Filters.eq(field1, value1));
+        // 遍历迭代器
+        MongoCursor<Document> iterator = documents.iterator();
+        String res = null;
+        while (iterator.hasNext()) {
+            Document document = iterator.next();
+            res = document.getString(field2);
+        }
+        return res;
+    }
+
+    /**
+     * 根据非_id多条件查询(and)
+     */
+    public static String getValueByAnd(String dbName, String collectionName, String field1, String value1, String field2, String value2, String field3) {
+        // 选择库
+        MongoDatabase db = mongoClient.getDatabase(dbName);
+        // 选择集合
+        MongoCollection<Document> collection = db.getCollection(collectionName);
+        // 条件查询
+        FindIterable<Document> documents = collection.find(Filters.and(Filters.eq(field1, value1), Filters.eq(field2, value2)));
+        // 遍历迭代器
+        MongoCursor<Document> iterator = documents.iterator();
+        String res = null;
+        while (iterator.hasNext()) {
+            Document document = iterator.next();
+            res = document.getString(field3);
+        }
+        return res;
+    }
+
+    /**
+     * 根据非_id多条件查询(or)
+     */
+    public static String getValueByOr(String dbName, String collectionName, String field1, String value1, String field2, String value2, String field3) {
+        // 选择库
+        MongoDatabase db = mongoClient.getDatabase(dbName);
+        // 选择集合
+        MongoCollection<Document> collection = db.getCollection(collectionName);
+        // 条件查询,使用or要注意该字段值不能为空
+        FindIterable<Document> documents = collection.find(Filters.or(Filters.eq(field1, value1), Filters.eq(field2, value2)));
+        // 遍历迭代器
+        MongoCursor<Document> iterator = documents.iterator();
+        String res = null;
+        while (iterator.hasNext()) {
+            Document document = iterator.next();
+            res = document.getString(field3);
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
-        System.out.println(getDocumentById("WAR3", "users", "grubby", "mobile"));
+        System.out.println(getValue("WAR3", "users", "grubby", "mobile"));
     }
 }
