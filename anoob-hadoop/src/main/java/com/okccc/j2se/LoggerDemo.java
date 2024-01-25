91c967c9b857f1f943789da875e1dc8de60ce92a
okccc <1573976179@qq.com>
2024-01-25 10:04:34 +0800

    ...

--- anoob-hadoop/src/main/java/com/okccc/j2se/LoggerDemo.java
+++ anoob-hadoop/src/main/java/com/okccc/j2se/LoggerDemo.java
@@ -9,12 +9,12 @@ import org.slf4j.LoggerFactory;
  * @Desc: java日志体系
  *
  * 日志接口
- * jcl(2002)：apache commons logging
+ * jcl(2002)：jakarta commons logging
  * slf4j(2006)：simple logging facade for java 简单日志门面,提供统一的日志接口,只包含slf4j-api.jar,没有具体实现
  *
  * 日志实现
- * jul(2002)：jdk自带
  * log4j(2001)：没有实现slf4j接口,需添加适配器slf4j-log4j12.jar将其和slf4j接口绑定
+ * jul(2002)：java.util.logging
  * logback(2006)：直接实现slf4j接口,是log4j的改良版,spring-boot默认配置就是slf4j + logback
  * log4j2(2012)：借鉴slf4j + logback所有特性并做了分离设计,log4j-api是日志接口,log4j-core是日志实现,异步IO性能最好
  * 可以在启动脚本中添加配置 -Dlog4j2.contextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector
