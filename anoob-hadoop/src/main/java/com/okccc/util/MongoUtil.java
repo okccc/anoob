3676391d8d38180803bab156b54187ab3c2b287a
okccc <1573976179@qq.com>
2024-08-17 09:35:01 +0800

    ...

--- anoob-hadoop/src/main/java/com/okccc/util/MongoUtil.java
+++ anoob-hadoop/src/main/java/com/okccc/util/MongoUtil.java
@@ -1,7 +1,5 @@
 package com.okccc.util;
 
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
 import com.mongodb.client.*;
 import com.mongodb.client.model.Filters;
 import org.bson.Document;
@@ -19,7 +17,8 @@ public class MongoUtil {
         String uri = "mongodb://root:root@localhost:27017/WAR3";
         try {
             // 创建mongo客户端
-            mongoClient = new MongoClient(new MongoClientURI(uri));
+//            mongoClient = new MongoClient(new MongoClientURI(uri));
+            mongoClient = MongoClients.create(uri);
             // 静态代码块随着类加载而加载,只执行一次,查看打印次数可以验证,main方法结束后会立即释放内存,下次调用会再次执行静态代码块
             System.out.println("create mongo client success!");
         } catch (Exception e) {
