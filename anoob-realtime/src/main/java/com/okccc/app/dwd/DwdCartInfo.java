449e45cec5473b363d1bfd732efe9d0430f7fe0e
okccc <1573976179@qq.com>
2025-03-02 10:06:22 +0800

    ...

--- anoob-realtime/src/main/java/com/okccc/app/dwd/DwdCartInfo.java
+++ anoob-realtime/src/main/java/com/okccc/app/dwd/DwdCartInfo.java
@@ -12,14 +12,14 @@ import java.time.Duration;
  * @Date: 2023/5/6 14:37:08
  * @Desc: 添加购物车(FlinkSQL - KafkaSource & MysqlSource - Lookup Join - KafkaSink)
  *
- * https://nightlies.apache.org/flink/flink-docs-release-1.17/docs/connectors/table/kafka/
- * https://nightlies.apache.org/flink/flink-docs-release-1.17/docs/connectors/table/jdbc/
- * https://nightlies.apache.org/flink/flink-docs-release-1.17/docs/connectors/table/jdbc/#lookup-cache
- * https://nightlies.apache.org/flink/flink-docs-release-1.17/docs/connectors/table/jdbc/#data-type-mapping
- * https://nightlies.apache.org/flink/flink-docs-release-1.17/docs/dev/configuration/advanced/#anatomy-of-table-dependent1es
- * https://nightlies.apache.org/flink/flink-docs-release-1.17/docs/dev/table/types/
- * https://nightlies.apache.org/flink/flink-docs-release-1.17/docs/dev/table/sql/queries/joins/#lookup-join
- * https://nightlies.apache.org/flink/flink-docs-release-1.17/docs/dev/table/concepts/time_attributes/#processing-time
+ * https://nightlies.apache.org/flink/flink-docs-release-1.20/docs/connectors/table/kafka/
+ * https://nightlies.apache.org/flink/flink-docs-release-1.20/docs/connectors/table/jdbc/
+ * https://nightlies.apache.org/flink/flink-docs-release-1.20/docs/connectors/table/jdbc/#lookup-cache
+ * https://nightlies.apache.org/flink/flink-docs-release-1.20/docs/connectors/table/jdbc/#data-type-mapping
+ * https://nightlies.apache.org/flink/flink-docs-release-1.20/docs/dev/configuration/advanced/#anatomy-of-table-dependent1es
+ * https://nightlies.apache.org/flink/flink-docs-release-1.20/docs/dev/table/types/
+ * https://nightlies.apache.org/flink/flink-docs-release-1.20/docs/dev/table/sql/queries/joins/#lookup-join
+ * https://nightlies.apache.org/flink/flink-docs-release-1.20/docs/dev/table/concepts/time_attributes/#processing-time
  *
  * lookup join
  * A lookup join is typically used to enrich a table with data that is queried from an external system.
