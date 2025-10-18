36a3e52bffb8d6a37756b671fee1458c0dfa7235
okccc <1573976179@qq.com>
2025-10-18 09:25:30 +0800

    ...

--- anoob-hadoop/src/main/java/com/okccc/util/ESUtil.java
+++ anoob-hadoop/src/main/java/com/okccc/util/ESUtil.java
@@ -1,6 +1,6 @@
 package com.okccc.util;
 
-import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson2.JSON;
 import com.okccc.bean.UserInfo;
 import org.apache.http.HttpHost;
 import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
