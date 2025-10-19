5ffa39e18a052c9885d82af26f5c978719e45fb2
okccc <1573976179@qq.com>
2025-10-19 09:25:30 +0800

    ...

--- anoob-hadoop/src/main/java/com/okccc/flume/interceptor/ETLInterceptor.java
+++ anoob-hadoop/src/main/java/com/okccc/flume/interceptor/ETLInterceptor.java
@@ -1,7 +1,7 @@
 package com.okccc.flume.interceptor;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.okccc.util.StringUtil;
 import org.apache.flume.Context;
 import org.apache.flume.Event;
