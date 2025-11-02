5db53ca791b3725eb3f3b206a6ffcea5a45015f4
okccc <1573976179@qq.com>
2025-11-02 17:42:50 +0800

    ...

--- anoob-hadoop/src/main/java/com/okccc/kafka/PartitionerDemo.java
+++ anoob-hadoop/src/main/java/com/okccc/kafka/PartitionerDemo.java
@@ -1,7 +1,7 @@
 package com.okccc.kafka;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import org.apache.kafka.clients.producer.Partitioner;
 import org.apache.kafka.common.Cluster;
 import org.apache.kafka.common.PartitionInfo;
