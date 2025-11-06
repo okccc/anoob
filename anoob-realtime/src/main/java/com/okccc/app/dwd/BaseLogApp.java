d52369e9382edbe6b3e28854b90c264ef7a1102d
okccc <1573976179@qq.com>
2025-11-06 09:46:57 +0800

    ...

--- anoob-realtime/src/main/java/com/okccc/app/dwd/BaseLogApp.java
+++ anoob-realtime/src/main/java/com/okccc/app/dwd/BaseLogApp.java
@@ -1,8 +1,8 @@
 package com.okccc.app.dwd;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONArray;
+import com.alibaba.fastjson2.JSONObject;
 import com.okccc.func.CheckUserFunction;
 import com.okccc.util.FlinkUtil;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
