8e6fc36471223ba00ea73e84426d5ba9919bae26
okccc <1573976179@qq.com>
2025-11-08 09:40:54 +0800

    ...

--- anoob-realtime/src/main/java/com/okccc/app/dim/DimApp.java
+++ anoob-realtime/src/main/java/com/okccc/app/dim/DimApp.java
@@ -1,7 +1,7 @@
 package com.okccc.app.dim;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.okccc.app.bean.TableProcess;
 import com.okccc.func.DimSinkFunction;
 import com.okccc.func.TableProcessFunction;
