e187133353f5701770edeb89b926b2bc45b876bd
okccc <1573976179@qq.com>
2025-11-12 09:46:15 +0800

    ...

--- anoob-realtime/src/main/java/com/okccc/util/DimUtil.java
+++ anoob-realtime/src/main/java/com/okccc/util/DimUtil.java
@@ -1,7 +1,7 @@
 package com.okccc.util;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import redis.clients.jedis.Jedis;
 
 /**
