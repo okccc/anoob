2e6b6c6ef7a43d1cf6f83968e3203cb25c1e3e84
okccc <1573976179@qq.com>
2025-11-13 14:07:42 +0800

    ...

--- anoob-realtime/src/main/java/com/okccc/app/ods/OdsApp.java
+++ anoob-realtime/src/main/java/com/okccc/app/ods/OdsApp.java
@@ -1,8 +1,7 @@
 package com.okccc.app.ods;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONAware;
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.okccc.util.FlinkUtil;
 import com.ververica.cdc.connectors.mysql.source.MySqlSource;
 import com.ververica.cdc.connectors.mysql.source.MySqlSourceBuilder;
@@ -160,7 +159,7 @@ public class OdsApp {
 
         // 5.往kafka写数据
         keyedStream
-                .map(JSONAware::toJSONString)
+                .map(value -> value.toJSONString())
                 .sinkTo(FlinkUtil.getKafkaSink("ods_base_db", tableType + "_mysql"))
                 .uid(tableType + "_sink");
     }
