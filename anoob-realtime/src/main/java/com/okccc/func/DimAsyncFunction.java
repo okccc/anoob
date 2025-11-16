40f93435d143e731697de9331d50d6b6e61bcd83
okccc <1573976179@qq.com>
2025-11-16 09:02:08 +0800

    ...

--- anoob-realtime/src/main/java/com/okccc/func/DimAsyncFunction.java
+++ anoob-realtime/src/main/java/com/okccc/func/DimAsyncFunction.java
@@ -1,6 +1,6 @@
 package com.okccc.func;
 
-import com.alibaba.fastjson.JSONObject;
+import com.alibaba.fastjson2.JSONObject;
 import com.okccc.util.DimUtil;
 import com.okccc.util.ThreadPoolUtil;
 import org.apache.flink.configuration.Configuration;
